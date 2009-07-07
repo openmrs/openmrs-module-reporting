8384daa716086c8971ba33075ce0d5afe5e9822b
Justin Miranda <jcm62@columbia.edu>
2009-07-07 15:04:40 +0000

    reporting:  Moved SerializationTest to its own package.

--- test/api/org/openmrs/module/serialization/SerializationTest.java
+++ test/api/org/openmrs/module/serialization/SerializationTest.java
@@ -11,7 +11,7 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.cohort;
+package org.openmrs.module.serialization;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
