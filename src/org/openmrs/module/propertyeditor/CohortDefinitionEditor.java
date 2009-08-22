42c7aca05c2f1d3dc27221ae6a01f927560452c8
Justin Miranda <jcm62@columbia.edu>
2009-08-22 19:55:41 +0000

    reporting:  Added property editors for all report objects.

---
+++ src/org/openmrs/module/propertyeditor/CohortDefinitionEditor.java
@@ -0,0 +1,56 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.propertyeditor;
+
+import java.beans.PropertyEditorSupport;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.cohort.definition.CohortDefinition;
+import org.openmrs.module.cohort.definition.service.CohortDefinitionService;
+import org.springframework.util.StringUtils;
+
+public class CohortDefinitionEditor extends PropertyEditorSupport {
+	
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	public CohortDefinitionEditor() {
+	}
+	
+	public void setAsText(String text) throws IllegalArgumentException {
+		CohortDefinitionService cds = Context.getService(CohortDefinitionService.class);
+		if (StringUtils.hasText(text)) {
+			try {
+				setValue(cds.getCohortDefinitionByUuid(text));
+			}
+			catch (Exception ex) {
+				log.error("Error setting text" + text, ex);
+				throw new IllegalArgumentException("CohortDefinition not found: " + ex.getMessage());
+			}
+		} else {
+			setValue(null);
+		}
+	}
+	
+	public String getAsText() {
+		CohortDefinition cd = (CohortDefinition) getValue();
+		if (cd == null) {
+			return "";
+		} else {
+			return cd.getUuid().toString();
+		}
+	}
+	
+}
