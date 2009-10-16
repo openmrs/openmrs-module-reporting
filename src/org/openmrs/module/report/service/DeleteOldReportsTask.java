aa9a9e73ec88dc23daa77de9743693f12ff6ff5b
Darius Jazayeri <jazayeri@alum.mit.edu>
2009-10-16 18:01:16 +0000

    reporting module: hide pointless error message when trying to run DeleteOldReportsTask when the module is not loaded

--- src/org/openmrs/module/report/service/DeleteOldReportsTask.java
+++ src/org/openmrs/module/report/service/DeleteOldReportsTask.java
@@ -2,6 +2,7 @@ package org.openmrs.module.report.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
 import org.openmrs.scheduler.tasks.AbstractTask;
 import org.openmrs.scheduler.tasks.ProcessHL7InQueueTask;
@@ -23,6 +24,8 @@ public class DeleteOldReportsTask extends AbstractTask {
 					authenticate();
 				}
 				Context.getService(ReportService.class).deleteOldReportRequests();
+			} catch (APIException ex) {
+				log.debug("Task not executed because module is not loaded");
 			} finally {
 				log.debug("Finishing DeleteOldReportsTask");
 				isExecuting = false;
