44a2682c68fe5e6b83cf489ecc84d76d551bddad
Darius Jazayeri <jazayeri@alum.mit.edu>
2010-03-30 16:52:53 +0000

    reporting: fixing some unit tests

--- test/api/org/openmrs/module/reporting/dataset/DataExportDataSetPersisterTest.java
+++ test/api/org/openmrs/module/reporting/dataset/DataExportDataSetPersisterTest.java
@@ -19,6 +19,8 @@ import junit.framework.Assert;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.dataset.definition.DataExportDataSetDefinition;
@@ -38,6 +40,60 @@ public class DataExportDataSetPersisterTest extends BaseModuleContextSensitiveTe
 	/** Logger */
 	private static Log log = LogFactory.getLog(DataExportDataSetPersisterTest.class);
 
+	@Before
+	public void setupInitialData() {
+		{
+			DataExportReportObject ro = new DataExportReportObject();
+			ro.setName("Test 1");
+			ro.setUuid("e3f05ec6-3c1b-11df-8f16-001e378eb67e");
+			ro.setDescription("Description 1");
+			ro.addSimpleColumn("patientId", "patientId");
+			ro.addConceptColumn("weight", "LAST", 1, "5089", null);
+			Context.getReportObjectService().saveReportObject(ro);
+		}
+		{
+			DataExportReportObject ro = new DataExportReportObject();
+			ro.setName("Test 2");
+			ro.setDescription("Description 2");
+			ro.addSimpleColumn("patientId", "patientId");
+			ro.addConceptColumn("cd4", "LAST", 1, "5497", null);
+			Context.getReportObjectService().saveReportObject(ro);
+		}
+		{
+			DataExportReportObject ro = new DataExportReportObject();
+			ro.setName("Test 3");
+			ro.setDescription("Description 3");
+			ro.addSimpleColumn("patientId", "patientId");
+			ro.addSimpleColumn("name", "personName");
+			Context.getReportObjectService().saveReportObject(ro);
+		}
+		{
+			DataExportReportObject ro = new DataExportReportObject();
+			ro.setName("Test 4");
+			ro.setDescription("Description 4");
+			ro.addSimpleColumn("patientId", "patientId");
+			ro.addConceptColumn("weight", "FIRST", 3, "5089", null);
+			Context.getReportObjectService().saveReportObject(ro);
+		}
+		{
+			DataExportReportObject ro = new DataExportReportObject();
+			ro.setName("Test 5");
+			ro.setDescription("Description 5");
+			ro.addSimpleColumn("patientId", "patientId");
+			ro.addSimpleColumn("gender", "patient.gender");
+			Context.getReportObjectService().saveReportObject(ro);
+		}
+	}
+
+	@Test
+	public void shouldReturnDataSetDefinitionIfExists() throws Exception {
+		// big time hack: this test only works if it's first, because I'm creating the Data Exports programmatically, and each run they get new autonumbered ids.
+		// if the initial data gets set up via executing an xml dataset instead, then this test can be anywhere in the class
+		DataSetDefinitionPersister persister = new DataExportDataSetDefinitionPersister();
+		DataSetDefinition def = persister.getDataSetDefinition(1);
+		Assert.assertNotNull("Should return dataset definition if exists", def); 		
+	}
+
 	/**
 	 * Tests whether a class registered with this persister can be handled.
 	 * 
@@ -46,8 +102,9 @@ public class DataExportDataSetPersisterTest extends BaseModuleContextSensitiveTe
 	 * @throws Exception
 	 */
 	@Test
+	@Ignore
 	public void shouldHandleRegisteredClass() throws Exception { 
-		//Assert.fail("Not implemented");		
+		Assert.fail("Not implemented");		
 	}
 	
 	/**
@@ -56,8 +113,9 @@ public class DataExportDataSetPersisterTest extends BaseModuleContextSensitiveTe
 	 * @throws Exception
 	 */
 	@Test
+	@Ignore
 	public void shouldNotHandleUnregisteredClass() throws Exception { 
-		//Assert.fail("Not implemented");		
+		Assert.fail("Not implemented");		
 	}
 	
 	/**
@@ -69,34 +127,28 @@ public class DataExportDataSetPersisterTest extends BaseModuleContextSensitiveTe
 	public void shouldGetAllDataSetDefinitions() throws Exception { 
 		DataSetDefinitionPersister persister = new DataExportDataSetDefinitionPersister();
 		List<DataSetDefinition> list = persister.getAllDataSetDefinitions(false);
-		Assert.assertEquals("Should return all dataset definitions", 25, list.size()); 		
+		Assert.assertEquals("Should return all dataset definitions", 5, list.size()); 		
 	}
 	
 	@Test
 	public void shouldReturnAllDatasetDefinitionsIncludingRetired() throws Exception { 
 		DataSetDefinitionPersister persister = new DataExportDataSetDefinitionPersister();
 		List<DataSetDefinition> list = persister.getAllDataSetDefinitions(true);
-		Assert.assertEquals("Should return all dataset definitions, including retired", 25, list.size()); 		
+		Assert.assertEquals("Should return all dataset definitions, including retired", 5, list.size()); 		
 	}
 	
 	@Test
 	public void shouldReturnAllDatasetDefinitionsExcludingRetired() throws Exception { 
 		DataSetDefinitionPersister persister = new DataExportDataSetDefinitionPersister();
 		List<DataSetDefinition> list = persister.getAllDataSetDefinitions(false);		
-		Assert.assertEquals("Should return all dataset definitions, excluding retired", 25, list.size()); 		
+		Assert.assertEquals("Should return all dataset definitions, excluding retired", 5, list.size()); 		
 	}
 	
 	@Test
 	public void shouldReturnNullIfNotExist() throws Exception { 
 		DataSetDefinitionPersister persister = new DataExportDataSetDefinitionPersister();
-		DataSetDefinition def = persister.getDataSetDefinition(1);	
+		DataSetDefinition def = persister.getDataSetDefinition(999);	
 		Assert.assertNull("Should return null if not exists", def); 		
-	}
-	@Test
-	public void shouldReturnDataSetDefinitionIfExists() throws Exception { 
-		DataSetDefinitionPersister persister = new DataExportDataSetDefinitionPersister();
-		DataSetDefinition def = persister.getDataSetDefinition(45);
-		Assert.assertNotNull("Should return dataset definition if exists", def); 		
 	}
 	@Test
 	public void shouldReturnNullIfNameDoesNotMatch() throws Exception { 
