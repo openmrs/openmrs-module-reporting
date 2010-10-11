177294714d2ab8d99e1a7bce42cdbd7f359a6bb8
Darius Jazayeri <jazayeri@alum.mit.edu>
2010-10-11 18:48:48 +0000

    adding a leading / to make displaying a logic data set definition work in tomcat (it already worked in jetty)

--- web/src/org/openmrs/module/reporting/web/controller/RenderLogicDataSetController.java
+++ web/src/org/openmrs/module/reporting/web/controller/RenderLogicDataSetController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RenderLogicDataSetController {
 
-	@RequestMapping("module/reporting/reports/renderLogicDataSet")
+	@RequestMapping("/module/reporting/reports/renderLogicDataSet")
 	public String showReport(Model model,
 	                       HttpSession session,
 	                       @RequestParam(required=false, value="start") Integer start,
