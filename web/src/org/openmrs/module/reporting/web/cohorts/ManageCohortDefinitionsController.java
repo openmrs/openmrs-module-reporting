f49e5a62406000d3aea2b2df08ad7ad637a2fee7
Wyclif Luyima <wyclif@openmrs.org>
2011-04-01 22:05:23 +0000

    Debug and implement Cohort Definition validation - report-98

--- web/src/org/openmrs/module/reporting/web/cohorts/ManageCohortDefinitionsController.java
+++ web/src/org/openmrs/module/reporting/web/cohorts/ManageCohortDefinitionsController.java
@@ -32,141 +32,144 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes({"cohortDefinition", "configurationProperties", "groupedProperties"})
+@SessionAttributes({ "cohortDefinition", "configurationProperties", "groupedProperties" })
 public class ManageCohortDefinitionsController {
 	
 	protected static Log log = LogFactory.getLog(ManageCohortDefinitionsController.class);
 	
-    /**
-     * Basically acts as the formBackingObject() method for saving a 
-     * cohort definition.
-     * 
-     * @param uuid
-     * @param type
-     * @param returnUrl
-     * @param model
-     * @return
-     */
-    @RequestMapping("/module/reporting/cohorts/editCohortDefinition")
-    public String editCohortDefinition(
-    		@RequestParam(required=false, value="uuid") String uuid,
-            @RequestParam(required=false, value="type") Class<? extends CohortDefinition> type,
-    		ModelMap model) {
-    	
-    	CohortDefinitionService service = Context.getService(CohortDefinitionService.class);
-    	CohortDefinition cd = service.getDefinition(uuid, type);
-     	model.addAttribute("cohortDefinition", cd);
-
-     	List<Property> properties = DefinitionUtil.getConfigurationProperties(cd);
-     	model.addAttribute("configurationProperties", properties);
-     	Map<String, List<Property>> groups = new LinkedHashMap<String, List<Property>>();
-     	for (Property p : properties) {
-     		List<Property> l = groups.get(p.getGroup());
-     		if (l == null) {
-     			l = new ArrayList<Property>();
-     			groups.put(p.getGroup(), l);
-     		}
-     		l.add(p);
-     	}
-     	model.addAttribute("groupedProperties", groups);
+	/**
+	 * Basically acts as the formBackingObject() method for saving a cohort definition.
+	 * 
+	 * @param uuid
+	 * @param type
+	 * @param returnUrl
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/module/reporting/cohorts/editCohortDefinition")
+	public String editCohortDefinition(@RequestParam(required = false, value = "uuid") String uuid,
+	                                   @RequestParam(required = false, value = "type") Class<? extends CohortDefinition> type,
+	                                   ModelMap model) {
+		
+		CohortDefinitionService service = Context.getService(CohortDefinitionService.class);
+		CohortDefinition cd = service.getDefinition(uuid, type);
+		model.addAttribute("cohortDefinition", cd);
+		
+		List<Property> properties = DefinitionUtil.getConfigurationProperties(cd);
+		model.addAttribute("configurationProperties", properties);
+		Map<String, List<Property>> groups = new LinkedHashMap<String, List<Property>>();
+		for (Property p : properties) {
+			List<Property> l = groups.get(p.getGroup());
+			if (l == null) {
+				l = new ArrayList<Property>();
+				groups.put(p.getGroup(), l);
+			}
+			l.add(p);
+		}
+		model.addAttribute("groupedProperties", groups);
+		
+		return "/module/reporting/cohorts/cohortDefinitionEditor";
+	}
 	
-        return "/module/reporting/cohorts/cohortDefinitionEditor";
-    }
-    
-    /**
-     * Saves a cohort definition.
-     * 
-     * @param uuid
-     * @param type
-     * @param name
-     * @param description
-     * @param model
-     * @return
-     */
-    @RequestMapping("/module/reporting/cohorts/saveCohortDefinition")
-    @SuppressWarnings("unchecked")
-    public String saveCohortDefinition(
-    		@RequestParam(required=false, value="uuid") String uuid,
-            @RequestParam(required=false, value="type") Class<? extends CohortDefinition> type,
-            @RequestParam(required=true, value="name") String name,
-            @RequestParam(required=false, value="description") String description,
-            HttpServletRequest request,
-            @ModelAttribute("cohortDefinition") CohortDefinition cohortDefinition,
-            BindingResult bindingResult,
-    		ModelMap model
-    ) {
-    	
-    	cohortDefinition.setName(name);
-    	cohortDefinition.setDescription(description);
-    	cohortDefinition.getParameters().clear();
-    	
-    	for (Property p : DefinitionUtil.getConfigurationProperties(cohortDefinition)) {
-    		String fieldName = p.getField().getName();
-    		String prefix = "parameter." + fieldName;
-    		String valParamName =  prefix + ".value"; 
-    		boolean isParameter = "t".equals(request.getParameter(prefix+".allowAtEvaluation"));
-    		
-    		Object valToSet = WidgetUtil.getFromRequest(request, valParamName, p.getField());
-    		
-    		Class<? extends Collection<?>> collectionType = null;
-    		Class<?> fieldType = p.getField().getType();   		
-			if (ReflectionUtil.isCollection(p.getField())) {
-				collectionType = (Class<? extends Collection<?>>)p.getField().getType();
-				fieldType = (Class<?>)ReflectionUtil.getGenericTypes(p.getField())[0];
+	/**
+	 * Saves a cohort definition.
+	 * 
+	 * @param uuid
+	 * @param type
+	 * @param name
+	 * @param description
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/module/reporting/cohorts/saveCohortDefinition")
+	@SuppressWarnings("unchecked")
+	public String saveCohortDefinition(@RequestParam(required = false, value = "uuid") String uuid,
+	                                   @RequestParam(required = false, value = "type") Class<? extends CohortDefinition> type,
+	                                   @RequestParam(required = true, value = "name") String name,
+	                                   @RequestParam(required = false, value = "description") String description,
+	                                   HttpServletRequest request,
+	                                   @ModelAttribute("cohortDefinition") CohortDefinition cohortDefinition,
+	                                   BindingResult bindingResult, ModelMap model) {
+		
+		cohortDefinition.setName(name);
+		cohortDefinition.setDescription(description);
+		cohortDefinition.getParameters().clear();
+		for (Property p : DefinitionUtil.getConfigurationProperties(cohortDefinition)) {
+			String fieldName = p.getField().getName();
+			String prefix = "parameter." + fieldName;
+			String valParamName = prefix + ".value";
+			boolean isParameter = "t".equals(request.getParameter(prefix + ".allowAtEvaluation"));
+			try {
+				Object valToSet = WidgetUtil.getFromRequest(request, valParamName, p.getField());
+				Class<? extends Collection<?>> collectionType = null;
+				Class<?> fieldType = p.getField().getType();
+				if (ReflectionUtil.isCollection(p.getField())) {
+					collectionType = (Class<? extends Collection<?>>) p.getField().getType();
+					fieldType = (Class<?>) ReflectionUtil.getGenericTypes(p.getField())[0];
+				}
+				
+				if (isParameter) {
+					//skip primitive types since they already have a default value
+					if (!fieldType.isPrimitive()) {
+						ReflectionUtil.setPropertyValue(cohortDefinition, p.getField(), null);
+					} else {
+						//use the wrapper class type equivalent so that the WidgetTag doesn't fail
+						fieldType = ReflectionUtil.getWrapperMap().get(fieldType.getName());
+					}
+					String paramLabel = ObjectUtil.nvlStr(request.getParameter(prefix + ".label"), fieldName);
+					Parameter param = new Parameter(fieldName, paramLabel, fieldType, collectionType, valToSet);
+					cohortDefinition.addParameter(param);
+				} else {
+					ReflectionUtil.setPropertyValue(cohortDefinition, p.getField(), valToSet);
+				}
 			}
-			
-			if (isParameter) {
-				ReflectionUtil.setPropertyValue(cohortDefinition, p.getField(), null);
-				String paramLabel = ObjectUtil.nvlStr(request.getParameter(prefix + ".label"), fieldName);
-				Parameter param = new Parameter(fieldName, paramLabel, fieldType, collectionType, valToSet);
-				cohortDefinition.addParameter(param);
+			catch (NumberFormatException e) {
+				bindingResult.rejectValue(fieldName, "reporting.error.invalidNumber", "Only integers are allowed");
 			}
-			else {
-				ReflectionUtil.setPropertyValue(cohortDefinition, p.getField(), valToSet);
-			}
-    	}
-    	
-    	new CohortDefinitionValidator().validate(cohortDefinition, bindingResult);
-    	if(bindingResult.hasErrors())
-    		return "/module/reporting/cohorts/cohortDefinitionEditor";
-    	
-    	if("".equals(cohortDefinition.getUuid()))
-    		cohortDefinition.setUuid(null);
-    	
-    	log.warn("Saving: " + cohortDefinition);
-    	Context.getService(CohortDefinitionService.class).saveDefinition(cohortDefinition);
-
-        return "redirect:/module/reporting/definition/manageDefinitions.form?type="+CohortDefinition.class.getName();
-    }
-
-    
-    /**
-     * Evaluates a cohort definition given a uuid.
-     * 
-     * @param uuid
-     * @param type
-     * @param returnUrl
-     * @param model
-     * @return
-     * @throws EvaluationException 
-     */
-    @RequestMapping("/module/reporting/cohorts/evaluateCohortDefinition")
-    public String evaluateCohortDefinition(
-    		@RequestParam(required=false, value="uuid") String uuid,
-            @RequestParam(required=false, value="type") Class<? extends CohortDefinition> type,
-    		ModelMap model) throws EvaluationException {
-    	
-    	CohortDefinitionService service = Context.getService(CohortDefinitionService.class);
-    	CohortDefinition cohortDefinition = service.getDefinition(uuid, type);
-     	
-    	// Evaluate the cohort definition
-    	EvaluationContext context = new EvaluationContext();
-    	Cohort cohort = service.evaluate(cohortDefinition, context);
-    	
-    	// create the model and view to return
-     	model.addAttribute("cohort", cohort);
-     	model.addAttribute("cohortDefinition", cohortDefinition);
-     	
-        return "/module/reporting/cohorts/cohortDefinitionEvaluator";
-    }
+		}
+		
+		if (bindingResult.hasErrors())
+			return "/module/reporting/cohorts/cohortDefinitionEditor";
+		
+		new CohortDefinitionValidator().validate(cohortDefinition, bindingResult);
+		if (bindingResult.hasErrors())
+			return "/module/reporting/cohorts/cohortDefinitionEditor";
+		
+		if ("".equals(cohortDefinition.getUuid()))
+			cohortDefinition.setUuid(null);
+		
+		log.warn("Saving: " + cohortDefinition);
+		Context.getService(CohortDefinitionService.class).saveDefinition(cohortDefinition);
+		
+		return "redirect:/module/reporting/definition/manageDefinitions.form?type=" + CohortDefinition.class.getName();
+	}
+	
+	/**
+	 * Evaluates a cohort definition given a uuid.
+	 * 
+	 * @param uuid
+	 * @param type
+	 * @param returnUrl
+	 * @param model
+	 * @return
+	 * @throws EvaluationException
+	 */
+	@RequestMapping("/module/reporting/cohorts/evaluateCohortDefinition")
+	public String evaluateCohortDefinition(@RequestParam(required = false, value = "uuid") String uuid,
+	                                       @RequestParam(required = false, value = "type") Class<? extends CohortDefinition> type,
+	                                       ModelMap model) throws EvaluationException {
+		
+		CohortDefinitionService service = Context.getService(CohortDefinitionService.class);
+		CohortDefinition cohortDefinition = service.getDefinition(uuid, type);
+		
+		// Evaluate the cohort definition
+		EvaluationContext context = new EvaluationContext();
+		Cohort cohort = service.evaluate(cohortDefinition, context);
+		
+		// create the model and view to return
+		model.addAttribute("cohort", cohort);
+		model.addAttribute("cohortDefinition", cohortDefinition);
+		
+		return "/module/reporting/cohorts/cohortDefinitionEvaluator";
+	}
 }
