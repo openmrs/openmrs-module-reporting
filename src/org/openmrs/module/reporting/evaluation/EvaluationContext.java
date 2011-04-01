b45167d04908155c608e45b7aca1ea52b61282bc
Wyclif Luyima <wyclif@openmrs.org>
2011-04-01 22:34:14 +0000

    NPE execption is thrown if any of the parameters of a definition is not set - REPORT-206

--- src/org/openmrs/module/reporting/evaluation/EvaluationContext.java
+++ src/org/openmrs/module/reporting/evaluation/EvaluationContext.java
@@ -21,12 +21,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Cohort;
 import org.openmrs.api.APIException;
+import org.openmrs.module.reporting.cohort.CohortUtil;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.evaluation.parameter.ParameterConstants;
 import org.openmrs.module.reporting.evaluation.parameter.ParameterException;
 import org.openmrs.module.reporting.evaluation.parameter.Parameterizable;
-import org.openmrs.module.reporting.cohort.CohortUtil;
 
 /**
  * The EvaluationContext provides the following capabilities: - A baseCohort, i.e. the universe of
@@ -38,7 +38,7 @@ import org.openmrs.module.reporting.cohort.CohortUtil;
 public class EvaluationContext {
 	
 	/* Logger */
-	protected static Log log = LogFactory.getLog(EvaluationContext.class);	
+	protected static Log log = LogFactory.getLog(EvaluationContext.class);
 	
 	// *******************
 	// PROPERTIES 
@@ -46,13 +46,13 @@ public class EvaluationContext {
 	
 	// Set a limit on the number of rows to evaluate 
 	private Integer limit = new Integer(0);
-
+	
 	// Base cohort to use for evaluation
-	private Cohort baseCohort;	
+	private Cohort baseCohort;
 	
 	// Parameter values entered by user (or defaulted)
 	private Map<String, Object> parameterValues;
-
+	
 	// Generic object cache
 	private transient Map<String, Object> cache;
 	
@@ -66,14 +66,14 @@ public class EvaluationContext {
 	/**
 	 * Default Constructor
 	 */
-	public EvaluationContext() { 
+	public EvaluationContext() {
 		this(new Date());
 	}
 	
 	/**
 	 * Constructor which sets the Evaluation Date to a particular date
 	 */
-	public EvaluationContext(Date evaluationDate) { 
+	public EvaluationContext(Date evaluationDate) {
 		this.evaluationDate = evaluationDate;
 		for (ParameterConstants c : ParameterConstants.values()) {
 			addParameterValue(c.getParameterName(), c.getParameterValue(this));
@@ -82,6 +82,7 @@ public class EvaluationContext {
 	
 	/**
 	 * Constructs a new EvaluationContext given the passed EvaluationContext
+	 * 
 	 * @param context
 	 */
 	public EvaluationContext(EvaluationContext context) {
@@ -98,6 +99,7 @@ public class EvaluationContext {
 	
 	/**
 	 * Clones an evaluation context and returns it
+	 * 
 	 * @param initialContext the EvaluationContext to clone
 	 * @return EvaluationContext the cloned EvaluationContext
 	 */
@@ -106,24 +108,26 @@ public class EvaluationContext {
 	}
 	
 	/**
-	 * Clone an EvaluationContext, replacing the parameters with those 
-	 * in the mapped child object as appropriate.
+	 * Clone an EvaluationContext, replacing the parameters with those in the mapped child object as
+	 * appropriate.
+	 * 
 	 * @param baseCohort
 	 * @param parameters
 	 * @return
 	 */
-	public static EvaluationContext cloneForChild(EvaluationContext initialContext, 
-												  Mapped<? extends Parameterizable> child) {
+	public static EvaluationContext cloneForChild(EvaluationContext initialContext, Mapped<? extends Parameterizable> child) {
 		
-		if (child == null || child.getParameterizable() == null) 
-			throw new APIException("The specified report could not be evaluated because one of its components has been removed from the database");
+		if (child == null || child.getParameterizable() == null)
+			throw new APIException(
+			        "The specified report could not be evaluated because one of its components has been removed from the database");
 		
 		EvaluationContext ec = EvaluationContext.clone(initialContext);
-			
+		
 		for (String paramName : child.getParameterMappings().keySet()) {
 			Parameter parameter = child.getParameterizable().getParameter(paramName);
 			if (parameter == null) {
-				throw new ParameterException("Cannot find parameter '" + paramName + "' in " + child.getParameterizable().getClass().getName() + " " + child.getParameterizable().getName());
+				throw new ParameterException("Cannot find parameter '" + paramName + "' in "
+				        + child.getParameterizable().getClass().getName() + " " + child.getParameterizable().getName());
 			}
 			Object paramVal = child.getParameterMappings().get(paramName);
 			if (paramVal instanceof String) {
@@ -144,13 +148,14 @@ public class EvaluationContext {
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("EvaluationContext[evaluationDate="+evaluationDate);
+		sb.append("EvaluationContext[evaluationDate=" + evaluationDate);
 		for (Map.Entry<String, Object> e : getParameterValues().entrySet()) {
-			sb.append("," + e.getKey() + "->" + e.getValue() + " (" + e.getValue().getClass().getSimpleName() + ")");
+			if (e.getValue() != null)
+				sb.append("," + e.getKey() + "->" + e.getValue() + " (" + e.getValue().getClass().getSimpleName() + ")");
 		}
 		sb.append("]");
 		return sb.toString();
-	}	
+	}
 	
 	/**
 	 * Get the cache property
@@ -223,7 +228,7 @@ public class EvaluationContext {
 		}
 		return parameterValues;
 	}
-
+	
 	/**
 	 * @param parameterValues the parameterValues to set
 	 */
@@ -233,6 +238,7 @@ public class EvaluationContext {
 	
 	/**
 	 * Returns true if a Parameter with this name has been added to the Context
+	 * 
 	 * @param parameterName the Parameter name to lookup
 	 * @return true if a Parameter with this name has been added to the Context
 	 */
@@ -242,6 +248,7 @@ public class EvaluationContext {
 	
 	/**
 	 * Add a parameter value for the given parameter name
+	 * 
 	 * @param parameterName
 	 * @param value
 	 */
@@ -251,12 +258,13 @@ public class EvaluationContext {
 	
 	/**
 	 * Retrieve a Parameter by Name
+	 * 
 	 * @param parameterName
 	 */
 	public Object getParameterValue(String parameterName) {
 		return getParameterValues().get(parameterName);
 	}
-
+	
 	/**
 	 * @return the baseCohort
 	 */
@@ -274,7 +282,7 @@ public class EvaluationContext {
 		clearCache();
 		this.baseCohort = baseCohort;
 	}
-
+	
 	/**
 	 * @return the number of rows to evaluate
 	 */
@@ -283,25 +291,25 @@ public class EvaluationContext {
 	}
 	
 	/**
-	 * @param the 
+	 * @param the
 	 */
 	public void setLimit(Integer limit) {
 		clearCache();
 		this.limit = limit;
 	}
-
+	
 	/**
 	 * @return the evaluationDate
 	 */
 	public Date getEvaluationDate() {
 		return evaluationDate;
 	}
-
+	
 	/**
 	 * @param evaluationDate the evaluationDate to set
 	 */
 	public void setEvaluationDate(Date evaluationDate) {
 		this.evaluationDate = evaluationDate;
 	}
-
+	
 }
