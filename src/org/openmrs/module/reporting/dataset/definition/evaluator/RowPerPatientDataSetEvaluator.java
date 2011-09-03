b54fc474f5ba022922d7c5529b1f1bb164db3806
Michael Seaton <mseaton@pih.org>
2011-09-03 01:52:41 +0000

    reporting: row per object branch.  Adding a few Evaluator unit tests

--- src/org/openmrs/module/reporting/dataset/definition/evaluator/RowPerPatientDataSetEvaluator.java
+++ src/org/openmrs/module/reporting/dataset/definition/evaluator/RowPerPatientDataSetEvaluator.java
@@ -17,41 +17,46 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.Person;
+import org.openmrs.Cohort;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.service.CohortDefinitionService;
 import org.openmrs.module.reporting.dataset.DataSet;
 import org.openmrs.module.reporting.dataset.definition.RowPerObjectDataSetDefinition;
-import org.openmrs.module.reporting.dataset.definition.RowPerPersonDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.RowPerPatientDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
-import org.openmrs.module.reporting.idset.EvaluatedIdSet;
 import org.openmrs.module.reporting.idset.IdSet;
-import org.openmrs.module.reporting.idset.PersonIdSet;
-import org.openmrs.module.reporting.idset.service.IdSetDefinitionService;
+import org.openmrs.module.reporting.idset.PatientIdSet;
 
 /**
- * The logic that evaluates a {@link RowPerObjectDataSetDefinition} and produces an {@link DataSet}
+ * The logic that evaluates a {@link RowPerPatientDataSetDefinition} and produces an {@link DataSet}
  */
-@Handler(supports=RowPerPersonDataSetDefinition.class)
-public class RowPerPersonDataSetEvaluator extends RowPerObjectDataSetEvaluator {
+@Handler(supports=RowPerPatientDataSetDefinition.class)
+public class RowPerPatientDataSetEvaluator extends RowPerObjectDataSetEvaluator {
 
 	protected Log log = LogFactory.getLog(this.getClass());
 
 	/**
 	 * Public constructor
 	 */
-	public RowPerPersonDataSetEvaluator() { }
+	public RowPerPatientDataSetEvaluator() { }
 	
 	/**
 	 * Implementations of this method should evaluate the appropriate id filters in the DataSetDefinition and
 	 * populate these IdSets within the Context
 	 */
 	public void populateFilterIdSets(RowPerObjectDataSetDefinition<?> dsd, EvaluationContext context) throws EvaluationException {
-		RowPerPersonDataSetDefinition rpp = (RowPerPersonDataSetDefinition) dsd;
-		if (rpp.getPersonFilter() != null) {
-			EvaluatedIdSet personIdSet = Context.getService(IdSetDefinitionService.class).evaluate(rpp.getPersonFilter(), context);
-			context.getBaseIdSets().put(Person.class, personIdSet.getIdSet());
+		RowPerPatientDataSetDefinition rpp = (RowPerPatientDataSetDefinition) dsd;
+		if (rpp.getPatientFilter() != null) {
+			EvaluatedCohort filterCohort = Context.getService(CohortDefinitionService.class).evaluate(rpp.getPatientFilter(), context);
+			if (context.getBaseCohort() == null) {
+				context.setBaseCohort(filterCohort);
+			}
+			else {
+				context.setBaseCohort(Cohort.intersect(context.getBaseCohort(), filterCohort));
+			}
 		}
 	}
 	
@@ -59,14 +64,17 @@ public class RowPerPersonDataSetEvaluator extends RowPerObjectDataSetEvaluator {
 	 * Implementations of this method should return the base IdSet that is appropriate for the passed DataSetDefinition
 	 */
 	public IdSet getBaseIdSet(RowPerObjectDataSetDefinition<?> dsd, EvaluationContext context) {
-		IdSet s = context.getBaseIdSets().get(Person.class);
-		if (s == null) {
-			s = new PersonIdSet();
-			String query = "select person_id from person where voided = false"; // TODO: Is this right?
+		PatientIdSet s = new PatientIdSet();
+		if (context.getBaseCohort() == null) {
+			s = new PatientIdSet();
+			String query = "select patient_id from patient where voided = false"; // TODO: Is this right?
 			List<List<Object>> results = Context.getAdministrationService().executeSQL(query, true);
 			for (List<Object> l : results) {
 				s.add((Integer)l.get(0));
 			}
+		}
+		else {
+			s = new PatientIdSet(context.getBaseCohort());
 		}
 		return s;
 	}
---
+++ test/api/org/openmrs/module/reporting/dataset/definition/evaluator/RowPerPersonDataSetEvaluatorTest.java
@@ -0,0 +1,50 @@
+package org.openmrs.module.reporting.dataset.definition.evaluator;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Person;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.dataset.RowPerObjectDataSet;
+import org.openmrs.module.reporting.dataset.column.definition.person.AgeColumnDefinition;
+import org.openmrs.module.reporting.dataset.column.definition.person.GenderColumnDefinition;
+import org.openmrs.module.reporting.dataset.column.definition.person.PersonIdColumnDefinition;
+import org.openmrs.module.reporting.dataset.definition.RowPerPersonDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.service.DataSetDefinitionService;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.idset.PersonIdSet;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+public class RowPerPersonDataSetEvaluatorTest extends BaseModuleContextSensitiveTest {
+	
+	@Before
+	public void setup() throws Exception {
+		executeDataSet("org/openmrs/module/reporting/include/ReportTestDataset.xml");
+	}
+	
+	@Test
+	public void evaluate_shouldEvaluateDataSetDefinition() throws Exception {
+		
+		RowPerPersonDataSetDefinition d = new RowPerPersonDataSetDefinition();
+		d.addColumnDefinition(new PersonIdColumnDefinition("Person ID"));
+		d.addColumnDefinition(new GenderColumnDefinition("Sexe"));
+		d.addColumnDefinition(new AgeColumnDefinition("Age"));
+		
+		EvaluationContext context = new EvaluationContext();
+		
+		RowPerObjectDataSet dataset = (RowPerObjectDataSet)Context.getService(DataSetDefinitionService.class).evaluate(d, context);
+		Assert.assertEquals("M", dataset.getColumnValue(2, "Sexe"));
+		Assert.assertEquals("F", dataset.getColumnValue(7, "Sexe"));
+		Assert.assertEquals(13, dataset.getRows().size());
+		
+		PersonIdSet personIdSet = new PersonIdSet();
+		personIdSet.add(2,6,8,501);
+		context.addIdSet(Person.class, personIdSet);
+		
+		dataset = (RowPerObjectDataSet)Context.getService(DataSetDefinitionService.class).evaluate(d, context);
+		Assert.assertEquals("M", dataset.getColumnValue(2, "Sexe"));
+		Assert.assertNull(dataset.getColumnValue(7, "Sexe"));
+		Assert.assertEquals(4, dataset.getRows().size());
+	}
+}
