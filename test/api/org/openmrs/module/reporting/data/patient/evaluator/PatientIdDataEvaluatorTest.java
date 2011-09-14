550695d670536be1e6df97737762c672976104a0
Michael Seaton <mseaton@pih.org>
2011-09-14 20:13:16 +0000

    rowperobjectdataset:  Tests for patient data evaluators

--- test/api/org/openmrs/module/reporting/data/patient/evaluator/PatientIdDataEvaluatorTest.java
+++ test/api/org/openmrs/module/reporting/data/patient/evaluator/PatientIdDataEvaluatorTest.java
@@ -2,15 +2,23 @@ package org.openmrs.module.reporting.data.patient.evaluator;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.Cohort;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.data.patient.EvaluatedPatientData;
 import org.openmrs.module.reporting.data.patient.definition.PatientDataDefinition;
 import org.openmrs.module.reporting.data.patient.definition.PatientIdDataDefinition;
 import org.openmrs.module.reporting.data.patient.service.PatientDataService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
 
-public class PatientIdDataEvaluatorTest {
+public class PatientIdDataEvaluatorTest extends BaseModuleContextSensitiveTest {
+	
+	@Before
+	public void setup() throws Exception {
+		executeDataSet("org/openmrs/module/reporting/include/ReportTestDataset.xml");
+	}
 	
 	/**
 	 * @see PatientIdDataEvaluator#evaluate(PatientDataDefinition,EvaluationContext)
@@ -19,9 +27,18 @@ public class PatientIdDataEvaluatorTest {
 	@Test
 	public void evaluate_shouldReturnPatientIdsForAllPatientsInTheThePassedContext() throws Exception {
 		
+		// Test for all patients
 		PatientIdDataDefinition d = new PatientIdDataDefinition();
 		EvaluationContext context = new EvaluationContext();
 		EvaluatedPatientData pd = Context.getService(PatientDataService.class).evaluate(d, context);
 		Assert.assertEquals(9, pd.getData().size());
+		for (Integer pId : pd.getData().keySet()) {
+			Assert.assertEquals(pId, pd.getData().get(pId));
+		}
+		
+		// Test for a limited base cohort of patients
+		context.setBaseCohort(new Cohort("2,6,7,8"));
+		pd = Context.getService(PatientDataService.class).evaluate(d, context);
+		Assert.assertEquals(4, pd.getData().size());
 	}
 }
--- test/api/org/openmrs/module/reporting/data/patient/evaluator/PatientIdentifierDataEvaluatorTest.java
+++ test/api/org/openmrs/module/reporting/data/patient/evaluator/PatientIdentifierDataEvaluatorTest.java
@@ -1,18 +1,43 @@
 package org.openmrs.module.reporting.data.patient.evaluator;
 
+import junit.framework.Assert;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.Cohort;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.data.patient.EvaluatedPatientData;
+import org.openmrs.module.reporting.data.patient.definition.PatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.patient.service.PatientDataService;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
 
-public class PatientIdentifierDataEvaluatorTest {
+public class PatientIdentifierDataEvaluatorTest extends BaseModuleContextSensitiveTest {
+	
+	@Before
+	public void setup() throws Exception {
+		executeDataSet("org/openmrs/module/reporting/include/ReportTestDataset.xml");
+	}
+	
 	/**
 	 * @see PatientIdentifierDataEvaluator#evaluate(PatientDataDefinition,EvaluationContext)
 	 * @verifies return the preferred identifier of the passed type for each patient in the passed context
 	 */
 	@Test
-	public void evaluate_shouldReturnThePreferredIdentifierOfThePassedTypeForEachPatientInThePassedContext()
-			throws Exception {
-		//TODO auto-generated
-		Assert.fail("Not yet implemented");
+	public void evaluate_shouldReturnThePreferredIdentifierOfThePassedTypeForEachPatientInThePassedContext() throws Exception {
+		
+		EvaluationContext context = new EvaluationContext();
+		context.setBaseCohort(new Cohort("2,6,7,8"));
+		
+		PatientIdentifierDataDefinition d = new PatientIdentifierDataDefinition();
+		d.setType(Context.getPatientService().getPatientIdentifierType(1));
+		EvaluatedPatientData pd = Context.getService(PatientDataService.class).evaluate(d, context);
+		
+		Assert.assertEquals(3, pd.getData().size()); // TODO: Is this what we want, or do we want all 4 patients returned, with potential null results?
+		Assert.assertEquals("101-6", pd.getData().get(2));
+		Assert.assertNull(pd.getData().get(6));
+		Assert.assertEquals("6TS-4", pd.getData().get(7));
+		Assert.assertEquals("7TU-8", pd.getData().get(8));
 	}
 }
