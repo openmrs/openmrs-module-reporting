75ce9c127a30e53b233c672eb5c715c6cfc75904
= Darius Jazayeri <jazayeri@alum.mit.edu>
2012-04-11 17:54:51 +0000

    If scheduler.password is wrong, then the reporting module will quickly lock your admin account - REPORT-368

--- src/org/openmrs/module/reporting/report/task/AbstractReportsTask.java
+++ src/org/openmrs/module/reporting/report/task/AbstractReportsTask.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.reporting.report.task;
 
+import java.util.Date;
 import java.util.TimerTask;
 
 import org.apache.commons.logging.Log;
@@ -15,6 +16,9 @@ import org.openmrs.scheduler.SchedulerConstants;
 public abstract class AbstractReportsTask extends TimerTask {
 	
 	private static Log log = LogFactory.getLog(AbstractReportsTask.class);
+	
+	// Per REPORT-368, we need to avoid locking the admin user account if the scheduler.password GP is wrong.
+	private static Date lastFailedLogin = null;
 	
 	/**
 	 * Sub-classes should override this method instead of the run method to implement their logic
@@ -48,14 +52,23 @@ public abstract class AbstractReportsTask extends TimerTask {
 	 * Authenticate the context so the task can call service layer.
 	 */
 	protected void authenticate() {
+		// Avoid locking the admin user account if the scheduler.password GP is wrong. 
+		// (The number 300000 is hardcoded in HibernateContextDAO in core as of 1.9. If that changes, we need to
+		// update this check. I add 5 seconds to that value to be sure we're safely out of the locking window.) 
+		if (lastFailedLogin != null && (System.currentTimeMillis() - lastFailedLogin.getTime() < 305000)) {
+			log.warn("Not authenticating to avoid locking account. Please ensure you scheduler username and password are configured correctly in your global properties");
+			return;
+		}
+		
+		AdministrationService adminService = Context.getAdministrationService();
+		String userName = adminService.getGlobalProperty(SchedulerConstants.SCHEDULER_USERNAME_PROPERTY);
+		String password = adminService.getGlobalProperty(SchedulerConstants.SCHEDULER_PASSWORD_PROPERTY);
 		try {
-			AdministrationService adminService = Context.getAdministrationService();
-			String userName = adminService.getGlobalProperty(SchedulerConstants.SCHEDULER_USERNAME_PROPERTY);
-			String password = adminService.getGlobalProperty(SchedulerConstants.SCHEDULER_PASSWORD_PROPERTY);
 			Context.authenticate(userName, password);
 		}
 		catch (ContextAuthenticationException e) {
-			log.error("Error authenticating user. Please ensure you scheduler username and password are configured correctly in your global properties", e);
+			log.warn("Error authenticating '" + userName + "' user. Please ensure you scheduler username and password are configured correctly in your global properties");
+			lastFailedLogin = new Date();
 		}
 	}
 }
