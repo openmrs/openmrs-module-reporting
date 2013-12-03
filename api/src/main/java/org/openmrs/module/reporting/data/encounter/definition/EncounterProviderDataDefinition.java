9bfa345a417b81ab08eff34518908ed1590bdcc4
mogoodrich <mgoodrich@pih.org>
2013-12-03 09:39:13 -0500

    REPORT-572: Add EncounterProviderDataDefinition and Evaluator

--- api/src/main/java/org/openmrs/module/reporting/data/encounter/definition/EncounterProviderDataDefinition.java
+++ api/src/main/java/org/openmrs/module/reporting/data/encounter/definition/EncounterProviderDataDefinition.java
@@ -13,42 +13,63 @@
  */
 package org.openmrs.module.reporting.data.encounter.definition;
 
-import org.openmrs.Person;
+import org.openmrs.Obs;
+import org.openmrs.OpenmrsMetadata;
 import org.openmrs.module.reporting.common.Localized;
 import org.openmrs.module.reporting.data.BaseDataDefinition;
-import org.openmrs.module.reporting.data.DataDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
 import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyCachingStrategy;
 import org.openmrs.module.reporting.evaluation.caching.Caching;
 
+import java.util.List;
+
 /**
- * Encounter Provider Column
+ * Encounter Provider Column (for 1.9.x and above)
  */
 @Caching(strategy=ConfigurationPropertyCachingStrategy.class)
 @Localized("reporting.EncounterProviderDataDefinition")
 public class EncounterProviderDataDefinition extends BaseDataDefinition implements EncounterDataDefinition {
 	
 	public static final long serialVersionUID = 1L;
-	
-	/**
-	 * Default Constructor
-	 */
+
+    @ConfigurationProperty
+    private OpenmrsMetadata encounterRole;
+
+    @ConfigurationProperty
+    private boolean singleObs = true;
+
 	public EncounterProviderDataDefinition() {
 		super();
 	}
-	
-	/**
-	 * Constructor to populate name only
-	 */
+
 	public EncounterProviderDataDefinition(String name) {
 		super(name);
 	}
 
-	//***** INSTANCE METHODS *****
-	
-	/** 
-	 * @see DataDefinition#getDataType()
-	 */
-	public Class<?> getDataType() {
-		return Person.class;
-	}
+    /**
+     * @return encounterRole will be of type EncounterRole.class
+     */
+    public OpenmrsMetadata getEncounterRole() {
+        return encounterRole;
+    }
+
+    /**
+     * @param encounterRole must be of type EncounterRole.class
+     */
+    public void setEncounterRole(OpenmrsMetadata encounterRole) {
+        this.encounterRole = encounterRole;
+    }
+
+    public boolean isSingleObs() {
+        return singleObs;
+    }
+
+    public void setSingleObs(boolean singleObs) {
+        this.singleObs = singleObs;
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return singleObs ? Obs.class : List.class;
+    }
 }
---
+++ api/src/main/java/org/openmrs/module/reporting/data/encounter/evaluator/EncounterProviderDataEvaluator.java
@@ -0,0 +1,93 @@
+package org.openmrs.module.reporting.data.encounter.evaluator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.openmrs.OpenmrsMetadata;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.reporting.data.encounter.EncounterDataUtil;
+import org.openmrs.module.reporting.data.encounter.EvaluatedEncounterData;
+import org.openmrs.module.reporting.data.encounter.definition.EncounterDataDefinition;
+import org.openmrs.module.reporting.data.encounter.definition.EncounterProviderDataDefinition;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Evaluates a EncounterProviderDataDefinition
+ * (Only works with OpenMRS 1.9 and above)
+ */
+@Handler(supports=EncounterProviderDataDefinition.class, order=50)
+public class EncounterProviderDataEvaluator implements EncounterDataEvaluator {
+
+
+    /**
+     * Logger
+     */
+    protected final Log log = LogFactory.getLog(getClass());
+
+
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    @Override
+    public EvaluatedEncounterData evaluate(EncounterDataDefinition definition, EvaluationContext context) throws EvaluationException {
+
+        EncounterProviderDataDefinition def = (EncounterProviderDataDefinition) definition;
+
+        // check to make sure parameter is an encounter role (method signature is type OpenmrsMetadata so it can compile again OpenMRS 1.6.x)
+        if (def.getEncounterRole() != null && !def.getEncounterRole().getClass().getName().equals("org.openmrs.EncounterRole")) {
+            throw new RuntimeException("EncounterRole parameter must be of type EncounterRole");
+        }
+
+        EvaluatedEncounterData data = new EvaluatedEncounterData(definition, context);
+
+        StringBuilder hql = new StringBuilder();
+        hql.append("select ep.encounter.id, ep.provider from EncounterProvider as ep ");
+        hql.append("where ep.encounter.id in (:ids) ");
+        
+        if (def.getEncounterRole() != null) {
+            hql.append("and ep.encounterRole.id = " + def.getEncounterRole().getId());
+        }
+
+        Set<Integer> encIds = EncounterDataUtil.getEncounterIdsForContext(context, false);
+        Query query = sessionFactory.getCurrentSession().createQuery(hql.toString());
+        query.setParameterList("ids", encIds);
+
+        // create an entry for each encounter
+        for (Integer encId : encIds) {
+            if (!def.isSingleObs()) {
+                data.addData(encId, new ArrayList<OpenmrsMetadata>());
+            }
+            else {
+                data.addData(encId, null);
+            }
+        }
+
+        // now populate with actual results
+        for (Object r : query.list()) {
+
+            Object[] result = (Object []) r;
+
+            if (!def.isSingleObs()) {
+                ((List<OpenmrsMetadata>) data.getData().get(result[0])).add((OpenmrsMetadata) result[1]);
+            }
+            else {
+
+                // note that if there are multiple matching obs and we are in singleObs mode then last one wins
+                if (data.getData().get(result[0]) != null) {
+                    log.warn("Multiple matching providers for encounter " + result[0] + "... picking one");
+                }
+
+                data.addData((Integer) result[0], result[1]);
+            }
+        }
+
+        return data;
+    }
+}
