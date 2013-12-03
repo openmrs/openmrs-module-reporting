df26335e37418ad8ec8fd1082a6894c4e2de6f2e
mogoodrich <mgoodrich@pih.org>
2013-12-03 18:35:48 -0500

    REPORT-574: Add ObsValueTextAsCodedConverter

---
+++ api/src/main/java/org/openmrs/module/reporting/data/converter/ObsValueTextAsCodedConverter.java
@@ -0,0 +1,49 @@
+package org.openmrs.module.reporting.data.converter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openmrs.Location;
+import org.openmrs.Obs;
+import org.openmrs.OpenmrsObject;
+import org.openmrs.api.context.Context;
+
+/**
+ * Interprets the valueText of an Obs as the id of an OpenMRS object, and converts
+ * to that object; currently only supports Locations
+ *
+ * @param <T>
+ */
+public class ObsValueTextAsCodedConverter<T extends OpenmrsObject> implements DataConverter {
+
+    private Class<T> dataType;
+
+    public ObsValueTextAsCodedConverter(Class<T> dataType) {
+    
+        if (dataType != Location.class) {
+            throw new IllegalArgumentException("ObValueTextAsCodedConverter only currently supports Location.class");
+        }
+    
+        this.dataType = dataType;
+    }
+
+    @Override
+    public Object convert(Object original) {
+
+        Obs obs = (Obs) original;
+
+        if (obs == null || StringUtils.isBlank(obs.getValueText())) {
+            return null;
+        }
+
+        return Context.getLocationService().getLocation(Integer.valueOf(obs.getValueText()));
+    }
+
+    @Override
+    public Class<?> getInputDataType() {
+        return Obs.class;
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return dataType;
+    }
+}
