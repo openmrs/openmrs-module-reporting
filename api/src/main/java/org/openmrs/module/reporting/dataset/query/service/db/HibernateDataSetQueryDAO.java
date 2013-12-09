openmrs/openmrs-module-reporting

commit 5a559a5ac39f2462c5822e58b2b7db9ccbc1aaf5

Performance improvements for certain data definitions that do "and patient_id in (:ids)" for very large cohorts

api/src/main/java/org/openmrs/module/reporting/dataset/query/service/db/HibernateDataSetQueryDAO.java
@@ -13,12 +13,6 @@
  */
 package org.openmrs.module.reporting.dataset.query.service.db;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Query;
@@ -37,8 +31,15 @@ import org.openmrs.PatientState;
 import org.openmrs.Person;
 import org.openmrs.PersonName;
 import org.openmrs.Relationship;
+import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.dataset.DataSetColumn;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class HibernateDataSetQueryDAO implements DataSetQueryDAO {
 
@@ -105,16 +106,21 @@ public class HibernateDataSetQueryDAO implements DataSetQueryDAO {
 		patientJoinProperties.put(Relationship.class, "personA.personId");
 		
 		String voidedProperty = (type == Person.class ? "personVoided" : "voided");
+
+        boolean filterInQuery = baseCohort != null && baseCohort.size() < ReportingConstants.MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE;
+        boolean doNotFilterInJava = baseCohort == null || filterInQuery;
 		
 		StringBuilder hql = new StringBuilder();
 		hql.append("select 	" + idPropertyName + ", " + property + " ");
 		hql.append("from	" + entityName + " " + alias + " ");
 		hql.append("where	" + alias + "." + voidedProperty + " = false ");
-		for (Class<? extends OpenmrsData> clazz : patientJoinProperties.keySet()) {
-			if (clazz.isAssignableFrom(type) && baseCohort != null) {
-				hql.append(" and " + alias + "." + patientJoinProperties.get(clazz) + " in (:ids)");
-			}
-		}
+        if (filterInQuery) {
+            for (Class<? extends OpenmrsData> clazz : patientJoinProperties.keySet()) {
+                if (clazz.isAssignableFrom(type) && baseCohort != null) {
+                    hql.append(" and " + alias + "." + patientJoinProperties.get(clazz) + " in (:ids)");
+                }
+            }
+        }
 		
 		Query query = sessionFactory.getCurrentSession().createQuery(hql.toString());
 		if(hql.toString().contains(":ids")) {
@@ -123,7 +129,10 @@ public class HibernateDataSetQueryDAO implements DataSetQueryDAO {
 		
 		for (Object o : query.list()) {
 			Object[] vals = (Object[]) o;
-			ret.put((Integer)vals[0], vals[1]);
+            Integer ptId = (Integer) vals[0];
+            if (doNotFilterInJava || baseCohort.contains(ptId)) {
+                ret.put(ptId, vals[1]);
+            }
 		}
 		return ret;
 	}

api/src/main/java/org/openmrs/module/reporting/ReportingConstants.java
@@ -13,15 +13,15 @@
  */
 package org.openmrs.module.reporting;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import org.openmrs.Location;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Constants required by this module
@@ -54,9 +54,12 @@ public class ReportingConstants {
 	public static final Parameter START_DATE_PARAMETER = new Parameter("startDate", "Start date", Date.class);
 	public static final Parameter END_DATE_PARAMETER = new Parameter("endDate", "End date", Date.class);
 	public static final Parameter LOCATION_PARAMETER = new Parameter("location", "Location", Location.class);
-	
-	
-	// Semi-Constants defined through global properties
+
+    // It is very slow to do "... AND patient_id IN (:cohort) for large cohorts. This is the cutoff we use to decide
+    // to filter in the database query versus filtering in Java
+    public static final int MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE = 2000;
+
+    // Semi-Constants defined through global properties
 	
 	public static final List<PatientIdentifierType> GLOBAL_PROPERTY_PREFERRED_IDENTIFIER_TYPES() {
 		String propertyValue = Context.getAdministrationService().getGlobalProperty("reporting.preferredIdentifierTypes");

api/src/main/java/org/openmrs/module/reporting/data/patient/evaluator/EncountersForPatientDataEvaluator.java
@@ -13,15 +13,12 @@
  */
 package org.openmrs.module.reporting.data.patient.evaluator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.openmrs.Cohort;
 import org.openmrs.Encounter;
 import org.openmrs.EncounterType;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.common.DateUtil;
 import org.openmrs.module.reporting.common.ListMap;
 import org.openmrs.module.reporting.common.TimeQualifier;
@@ -32,25 +29,33 @@ import org.openmrs.module.reporting.dataset.query.service.DataSetQueryService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Evaluates an EncountersForPatientDataDefinition to produce a PatientData
  */
 @Handler(supports=EncountersForPatientDataDefinition.class, order=50)
 public class EncountersForPatientDataEvaluator implements PatientDataEvaluator {
 
-	/** 
+    /**
 	 * @see PatientDataEvaluator#evaluate(PatientDataDefinition, EvaluationContext)
 	 * @should return the obs that match the passed definition configuration
 	 */
 	public EvaluatedPatientData evaluate(PatientDataDefinition definition, EvaluationContext context) throws EvaluationException {
-		
+        Cohort baseCohort = context.getBaseCohort();
+        boolean filterPatientsInQuery = baseCohort != null && baseCohort.size() < ReportingConstants.MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE;
+        boolean doNotFilterPatientsInJava = baseCohort == null || filterPatientsInQuery;
+
 		EncountersForPatientDataDefinition def = (EncountersForPatientDataDefinition) definition;
 		EvaluatedPatientData c = new EvaluatedPatientData(def, context);
 		
-		if (context.getBaseCohort() != null && context.getBaseCohort().isEmpty()) {
+		if (baseCohort != null && baseCohort.isEmpty()) {
 			return c;
 		}
-		
+
 		DataSetQueryService qs = Context.getService(DataSetQueryService.class);
 		
 		StringBuilder hql = new StringBuilder();
@@ -58,8 +63,8 @@ public class EncountersForPatientDataEvaluator implements PatientDataEvaluator {
 		
 		hql.append("from 		Encounter ");
 		hql.append("where 		voided = false ");
-		
-		if (context.getBaseCohort() != null) {
+
+        if (filterPatientsInQuery) {
 			hql.append("and 		patient.patientId in (:patientIds) ");
 			m.put("patientIds", context.getBaseCohort());
 		}
@@ -89,8 +94,10 @@ public class EncountersForPatientDataEvaluator implements PatientDataEvaluator {
 		
 		ListMap<Integer, Encounter> encsForPatients = new ListMap<Integer, Encounter>();
 		for (Object o : queryResult) {
-			Encounter e = (Encounter)o;
-			encsForPatients.putInList(e.getPatientId(), e);
+			Encounter e = (Encounter) o;
+            if (doNotFilterPatientsInJava || baseCohort.contains(e.getPatientId())) {
+			    encsForPatients.putInList(e.getPatientId(), e);
+            }
 		}
 		
 		for (Integer pId : encsForPatients.keySet()) {

api/src/main/java/org/openmrs/module/reporting/data/patient/evaluator/PatientIdentifierDataEvaluator.java
@@ -13,17 +13,12 @@
  */
 package org.openmrs.module.reporting.data.patient.evaluator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.openmrs.Cohort;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.common.ListMap;
 import org.openmrs.module.reporting.data.patient.EvaluatedPatientData;
 import org.openmrs.module.reporting.data.patient.definition.PatientDataDefinition;
@@ -31,6 +26,13 @@ import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDat
 import org.openmrs.module.reporting.dataset.query.service.DataSetQueryService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Evaluates a PatientIdentifierDataDefinition to produce a PatientData
@@ -57,32 +59,38 @@ public class PatientIdentifierDataEvaluator implements PatientDataEvaluator {
 		for (PatientIdentifierType t : def.getTypes()) {
 			idTypes.add(t.getPatientIdentifierTypeId());
 		}
-	
+
+        Cohort baseCohort = context.getBaseCohort();
+        boolean filterPatientsInQuery = baseCohort != null && baseCohort.size() < ReportingConstants.MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE;
+        boolean doNotFilterPatientsInJava = baseCohort == null || filterPatientsInQuery;
+
 		StringBuilder hql = new StringBuilder();
 		hql.append("select 		pi.patient.patientId, pi ");
 		hql.append("from 		PatientIdentifier as pi ");
 		hql.append("where 		voided = false ");
-		if (context.getBaseCohort() != null) {
-			hql.append("and 		patient.patientId in (:patientIds) ");
-		}
+        if (filterPatientsInQuery) {
+            hql.append("and 		patient.patientId in (:patientIds) ");
+        }
 		hql.append("and 		identifierType.patientIdentifierTypeId in (:idTypes) ");
 		hql.append("order by 	preferred asc");
 
 		Map<String, Object> m = new HashMap<String, Object>();
-		if (context.getBaseCohort() != null) {
+        if (filterPatientsInQuery) {
 			m.put("patientIds", context.getBaseCohort());
 		}
 		m.put("idTypes", idTypes);
 
 		List<Object> queryResult = qs.executeHqlQuery(hql.toString(), m);
-		
-		ListMap<Integer, PatientIdentifier> patIds = new ListMap<Integer, PatientIdentifier>();
+
+        ListMap<Integer, PatientIdentifier> patIds = new ListMap<Integer, PatientIdentifier>();
 		for (Object o : queryResult) {
 			Object[] parts = (Object[]) o;
 			if (parts.length == 2) {
 				Integer pId = (Integer) parts[0];
-				PatientIdentifier pi = (PatientIdentifier) parts[1];
-				patIds.putInList(pId, pi);
+                if (doNotFilterPatientsInJava || baseCohort.contains(pId)) {
+                    PatientIdentifier pi = (PatientIdentifier) parts[1];
+                    patIds.putInList(pId, pi);
+                }
 			}
 		}
 		

api/src/main/java/org/openmrs/module/reporting/data/person/evaluator/BirthdateDataEvaluator.java
@@ -13,13 +13,10 @@
  */
 package org.openmrs.module.reporting.data.person.evaluator;
 
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.openmrs.Person;
+import org.openmrs.Cohort;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.common.Birthdate;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
@@ -27,6 +24,12 @@ import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.dataset.query.service.DataSetQueryService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Evaluates a BirthdateDataDefinition to produce a PersonData
@@ -39,21 +42,39 @@ public class BirthdateDataEvaluator implements PersonDataEvaluator {
 	 * @should return all birth dates for all persons
 	 */
 	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        Cohort baseCohort = context.getBaseCohort();
+        boolean filterPatientsInQuery = baseCohort != null && baseCohort.size() < ReportingConstants.MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE;
+        boolean doNotFilterPatientsInJava = baseCohort == null || filterPatientsInQuery;
+
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
-		DataSetQueryService qs = Context.getService(DataSetQueryService.class);	
-		Map<Integer, Object> birthdateData = qs.getPropertyValues(Person.class, "birthdate", context);
-		Map<Integer, Object> estimatedData = qs.getPropertyValues(Person.class, "birthdateEstimated", context);
-		Map<Integer, Object> ret = new LinkedHashMap<Integer, Object>();
-		for (Integer pId : birthdateData.keySet()) {
-			Birthdate birthdate = null;
-			Date bd = (Date)birthdateData.get(pId);
-			if (bd != null) {
-				boolean estimated = estimatedData.get(pId) == Boolean.TRUE;
-				birthdate = new Birthdate(bd, estimated);
-			}
-			ret.put(pId, birthdate);
-		}
-		c.setData(ret);
-		return c;
+		DataSetQueryService qs = Context.getService(DataSetQueryService.class);
+
+        String hql = "select personId, birthdate, birthdateEstimated from Person where voided = false";
+        if (filterPatientsInQuery) {
+            hql += " and personId in (:patientIds)";
+        }
+        HashMap<String, Object> params = new HashMap<String, Object>();
+        if (filterPatientsInQuery) {
+            params.put("patientIds", baseCohort);
+        }
+
+        Map<Integer, Object> ret = new LinkedHashMap<Integer, Object>();
+        List<Object> queryResult = qs.executeHqlQuery(hql, params);
+        for (Object o : queryResult) {
+            Object[] parts = (Object[]) o;
+            Integer pId = (Integer) parts[0];
+            Birthdate birthdate = null;
+            if (doNotFilterPatientsInJava || baseCohort.contains(pId)) {
+                Date bd = (Date) parts[1];
+                if (bd != null) {
+                    boolean estimated = (Boolean) parts[2];
+                    birthdate = new Birthdate(bd, estimated);
+                }
+            }
+            ret.put(pId, birthdate);
+        }
+        c.setData(ret);
+        return c;
 	}
+
 }

api/src/main/java/org/openmrs/module/reporting/data/person/evaluator/PersonAttributeDataEvaluator.java
@@ -13,13 +13,11 @@
  */
 package org.openmrs.module.reporting.data.person.evaluator;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.openmrs.Cohort;
 import org.openmrs.PersonAttribute;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.data.patient.evaluator.PatientDataEvaluator;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.PersonAttributeDataDefinition;
@@ -27,6 +25,10 @@ import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.dataset.query.service.DataSetQueryService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Evaluates a PersonAttributeDataDefinition to produce a PatientData
@@ -48,23 +50,30 @@ public class PersonAttributeDataEvaluator implements PersonDataEvaluator {
 		}
 		
 		DataSetQueryService qs = Context.getService(DataSetQueryService.class);
+
+        Cohort baseCohort = context.getBaseCohort();
+        boolean filterPatientsInQuery = baseCohort != null && baseCohort.size() < ReportingConstants.MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE;
+        boolean doNotFilterPatientsInJava = baseCohort == null || filterPatientsInQuery;
 		
 		StringBuilder hql = new StringBuilder();
 		hql.append("from 		PersonAttribute ");
 		hql.append("where 		voided = false ");
-		if (context.getBaseCohort() != null) {
+		if (filterPatientsInQuery) {
 			hql.append("and 		person.personId in (:patientIds) ");
 		}
 		hql.append("and 		attributeType.personAttributeTypeId = :idType ");
 		Map<String, Object> m = new HashMap<String, Object>();
-		if (context.getBaseCohort() != null) {
+		if (filterPatientsInQuery) {
 			m.put("patientIds", context.getBaseCohort());
 		}
 		m.put("idType", def.getPersonAttributeType().getPersonAttributeTypeId());
 		List<Object> queryResult = qs.executeHqlQuery(hql.toString(), m);
 		for (Object o : queryResult) {
 			PersonAttribute pa = (PersonAttribute)o;
-			c.addData(pa.getPerson().getPersonId(), pa);  // TODO: This is probably inefficient.  Try to improve this with HQL
+            Integer personId = pa.getPerson().getPersonId(); // TODO: This is probably inefficient.  Try to improve this with HQL
+            if (doNotFilterPatientsInJava || baseCohort.contains(personId)) {
+                c.addData(personId, pa);
+            }
 		}
 		return c;
 	}

api/src/main/java/org/openmrs/module/reporting/data/person/evaluator/PreferredAddressDataEvaluator.java
@@ -13,19 +13,21 @@
  */
 package org.openmrs.module.reporting.data.person.evaluator;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.openmrs.Cohort;
 import org.openmrs.PersonAddress;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PreferredAddressDataDefinition;
 import org.openmrs.module.reporting.dataset.query.service.DataSetQueryService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Evaluates a PreferredAddressDataDefinition to produce a PersonData
@@ -38,12 +40,16 @@ public class PreferredAddressDataEvaluator implements PersonDataEvaluator {
 	 * @should return the preferred address for all persons
 	 */
 	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        Cohort baseCohort = context.getBaseCohort();
+        boolean filterPatientsInQuery = baseCohort != null && baseCohort.size() < ReportingConstants.MAX_PATIENT_IDS_TO_FILTER_IN_DATABASE;
+        boolean doNotFilterPatientsInJava = baseCohort == null || filterPatientsInQuery;
+
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		DataSetQueryService qs = Context.getService(DataSetQueryService.class);
 		Map<String, Object> m = new HashMap<String, Object>();
 		
 		String hql = "from PersonAddress where voided = false ";
-		if (context.getBaseCohort() != null) {
+		if (filterPatientsInQuery) {
 			hql += "and person.personId in (:personIds) ";
 			m.put("personIds", context.getBaseCohort());
 		}
@@ -51,9 +57,13 @@ public class PreferredAddressDataEvaluator implements PersonDataEvaluator {
 
 		List<Object> queryResult = qs.executeHqlQuery(hql, m);
 		for (Object o : queryResult) {
-			PersonAddress pa = (PersonAddress)o;
-			c.addData(pa.getPerson().getPersonId(), pa);  // TODO: This is probably inefficient.  Try to improve this
+			PersonAddress pa = (PersonAddress) o;
+            Integer pId = pa.getPerson().getPersonId(); // TODO: This is probably inefficient.  Try to improve this
+            if (doNotFilterPatientsInJava || baseCohort.contains(pId)) {
+                c.addData(pId, pa);
+            }
 		}
 		return c;
 	}
+
 }

api/src/main/java/org/openmrs/module/reporting/dataset/definition/evaluator/PatientDataSetEvaluator.java
@@ -82,18 +82,26 @@ public class PatientDataSetEvaluator implements DataSetEvaluator {
 
 		// Evaluate each specified ColumnDefinition for all of the included rows and add these to the dataset
 		for (RowPerObjectColumnDefinition cd : dsd.getColumnDefinitions()) {
-			
-			MappedData<? extends PatientDataDefinition> dataDef = (MappedData<? extends PatientDataDefinition>) cd.getDataDefinition();
-			EvaluatedPatientData data = Context.getService(PatientDataService.class).evaluate(dataDef, ec);
-			
-			for (Integer id : c.getMemberIds()) {
-				for (DataSetColumn column : cd.getDataSetColumns()) {
-					Object val = data.getData().get(id);
-					val = DataUtil.convertData(val, dataDef.getConverters());
-					dataSet.addColumnValue(id, column, val);
-				}
-			}
-		}
+
+            System.out.println("evaluating column: " + cd.getName());
+            long l = System.currentTimeMillis();
+
+            MappedData<? extends PatientDataDefinition> dataDef = (MappedData<? extends PatientDataDefinition>) cd.getDataDefinition();
+            EvaluatedPatientData data = Context.getService(PatientDataService.class).evaluate(dataDef, ec);
+
+            System.out.println("...took " + (System.currentTimeMillis() - l) + " ms");
+            l = System.currentTimeMillis();
+
+            for (Integer id : c.getMemberIds()) {
+                for (DataSetColumn column : cd.getDataSetColumns()) {
+                    Object val = data.getData().get(id);
+                    val = DataUtil.convertData(val, dataDef.getConverters());
+                    dataSet.addColumnValue(id, column, val);
+                }
+            }
+
+            System.out.println("...took " + (System.currentTimeMillis() - l) + " ms to set columns in dataset");
+        }
 
 		return dataSet;
 	}

commit 45d3445e2d3df19524c7503c99148a89b1147ed3

Fix a bug introduced in commit 5a559a5; only Patient and Person datasets can be filtered in Java based on cohort member ids.

api/src/main/java/org/openmrs/module/reporting/dataset/query/service/db/HibernateDataSetQueryDAO.java
@@ -18,27 +18,11 @@ import org.apache.commons.logging.LogFactory;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.metadata.ClassMetadata;
-import org.openmrs.Cohort;
-import org.openmrs.Encounter;
-import org.openmrs.Obs;
-import org.openmrs.OpenmrsData;
-import org.openmrs.OpenmrsObject;
-import org.openmrs.Order;
-import org.openmrs.Patient;
-import org.openmrs.PatientIdentifier;
-import org.openmrs.PatientProgram;
-import org.openmrs.PatientState;
-import org.openmrs.Person;
-import org.openmrs.PersonName;
-import org.openmrs.Relationship;
+import org.openmrs.*;
 import org.openmrs.module.reporting.dataset.DataSetColumn;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class HibernateDataSetQueryDAO implements DataSetQueryDAO {
 
@@ -106,7 +90,10 @@ public class HibernateDataSetQueryDAO implements DataSetQueryDAO {
 		
 		String voidedProperty = (type == Person.class ? "personVoided" : "voided");
 
-        boolean filterInQuery = baseCohort != null && baseCohort.size() < 2000;  // TODO: Change to batch if it is good
+        // the special-case code for filtering in Java if the baseCohort is too big only handles Person or Patient queries
+        // (though we may eventually want to extend it)
+        boolean personOrPatientQuery = type.equals(Patient.class) || type.equals(Person.class);
+        boolean filterInQuery = !personOrPatientQuery || (baseCohort != null && baseCohort.size() < 2000);  // TODO: Change to batch if it is good
         boolean doNotFilterInJava = baseCohort == null || filterInQuery;
 		
 		StringBuilder hql = new StringBuilder();
@@ -125,7 +112,7 @@ public class HibernateDataSetQueryDAO implements DataSetQueryDAO {
 		if(hql.toString().contains(":ids")) {
 			query.setParameterList("ids", baseCohort.getMemberIds());
 		}
-		
+
 		for (Object o : query.list()) {
 			Object[] vals = (Object[]) o;
             Integer ptId = (Integer) vals[0];