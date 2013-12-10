a1f2041790213dcc6816e74c1d482e355eda9371
djazayeri <jazayeri@alum.mit.edu>
2013-12-09 17:48:50 -0800

    REPORT-579 - Encounter data definition that gets a simultaneous encounter to the one in question

---
+++ api/src/main/java/org/openmrs/module/reporting/data/encounter/evaluator/SimultaneousEncountersDataEvaluator.java
@@ -0,0 +1,79 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.reporting.data.encounter.evaluator;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.reporting.data.encounter.EncounterDataUtil;
+import org.openmrs.module.reporting.data.encounter.EvaluatedEncounterData;
+import org.openmrs.module.reporting.data.encounter.definition.EncounterDataDefinition;
+import org.openmrs.module.reporting.data.encounter.definition.SimultaneousEncountersDataDefinition;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Set;
+
+@Handler(supports= SimultaneousEncountersDataDefinition.class, order=50)
+public class SimultaneousEncountersDataEvaluator implements EncounterDataEvaluator {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public EvaluatedEncounterData evaluate(EncounterDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        EvaluatedEncounterData results = new EvaluatedEncounterData(definition, context);
+
+        Set<Integer> encIds = EncounterDataUtil.getEncounterIdsForContext(context, true);
+
+        SimultaneousEncountersDataDefinition def = (SimultaneousEncountersDataDefinition) definition;
+
+        String hql = "select enc.id, adt " +
+                "from Encounter enc, Encounter adt " +
+                "where enc.encounterDatetime = adt.encounterDatetime " +
+                "  and enc.patient.id = adt.patient.id " +
+                "  and enc.id != adt.id " +
+                "  and enc.voided = false " +
+                "  and adt.voided = false ";
+        if (def.getEncounterTypeList() != null) {
+            if (def.getEncounterTypeList().isEmpty()) {
+                return results;
+            }
+            hql += "  and adt.encounterType in (:encounterTypes) ";
+        }
+        if (encIds != null) {
+            hql += "  and enc.id in (:encIds) ";
+        }
+        hql += "order by adt.dateCreated asc"; // use the most-recently-entered encounter
+        // hql += "order by ABS(adt.dateCreated - enc.dateCreated) desc"; // use the encounter with the nearest datetime to enc
+
+        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+        if (def.getEncounterTypeList() != null) {
+            query.setParameterList("encounterTypes", def.getEncounterTypeList());
+        }
+        if (encIds != null) {
+            query.setParameterList("encIds", encIds);
+        }
+
+        for (Object[] row : (List<Object[]>) query.list()) {
+            results.addData((Integer) row[0], row[1]);
+        }
+
+        return results;
+    }
+
+}
---
+++ api/src/test/java/org/openmrs/module/reporting/data/encounter/evaluator/SimultaneousEncountersDataEvaluatorTest.java
@@ -0,0 +1,61 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.reporting.data.encounter.evaluator;
+
+import org.junit.Test;
+import org.openmrs.Encounter;
+import org.openmrs.Patient;
+import org.openmrs.contrib.testdata.TestDataManager;
+import org.openmrs.module.reporting.data.encounter.EvaluatedEncounterData;
+import org.openmrs.module.reporting.data.encounter.definition.SimultaneousEncountersDataDefinition;
+import org.openmrs.module.reporting.data.encounter.service.EncounterDataService;
+import org.openmrs.module.reporting.evaluation.context.EncounterEvaluationContext;
+import org.openmrs.module.reporting.query.encounter.EncounterIdSet;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SimultaneousEncountersDataEvaluatorTest extends BaseModuleContextSensitiveTest {
+
+    @Autowired
+    TestDataManager testData;
+
+    @Autowired
+    EncounterDataService encounterDataService;
+
+    @Test
+    public void testEvaluate() throws Exception {
+        Patient p1 = testData.randomPatient().save();
+        Patient p2 = testData.randomPatient().save();
+
+        Encounter indexEncounter = testData.randomEncounter().patient(p1).save();
+        Encounter associated = testData.randomEncounter().patient(p1).encounterDatetime(indexEncounter.getEncounterDatetime()).save();
+        Encounter another = testData.randomEncounter().patient(p1).save();
+        Encounter otherPatient = testData.randomEncounter().patient(p2).encounterDatetime(indexEncounter.getEncounterDatetime()).save();
+
+        EncounterEvaluationContext context = new EncounterEvaluationContext();
+        context.setBaseEncounters(new EncounterIdSet(indexEncounter.getEncounterId()));
+
+        SimultaneousEncountersDataDefinition def = new SimultaneousEncountersDataDefinition();
+        def.addEncounterType(associated.getEncounterType());
+
+        EvaluatedEncounterData result = encounterDataService.evaluate(def, context);
+        assertThat(result.getData().size(), is(1));
+        assertThat((Encounter) result.getData().values().iterator().next(), is(associated));
+    }
+
+}
