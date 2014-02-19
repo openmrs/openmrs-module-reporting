0ca0327f500ebb10c35396b9393cbac244b80c48
Michael Seaton <mseaton@pih.org>
2014-02-19 12:34:14 -0500

    REPORT-611:  Create basic Map Data Converter

---
+++ api/src/main/java/org/openmrs/module/reporting/data/converter/MapConverter.java
@@ -0,0 +1,126 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.reporting.data.converter;
+
+import org.openmrs.module.reporting.common.ObjectUtil;
+
+import java.util.Map;
+
+/**
+ * Collection converter
+ */
+public class MapConverter implements DataConverter {
+
+	//***** PROPERTIES *****
+
+	private String keyValueSeparator;
+	private String entrySeparator;
+	private DataConverter keyConverter;
+	private DataConverter valueConverter;
+
+	//***** CONSTRUCTORS *****
+
+	/**
+	 * Default Constructor
+	 */
+	public MapConverter() { }
+
+	/**
+	 * Full Constructor
+	 */
+	public MapConverter(String keyValueSeparator, String entrySeparator, DataConverter keyConverter, DataConverter valueConverter) {
+		this.keyValueSeparator = keyValueSeparator;
+		this.entrySeparator = entrySeparator;
+		this.keyConverter = keyConverter;
+		this.valueConverter = valueConverter;
+	}
+
+
+	//***** INSTANCE METHODS *****
+
+	/**
+	 * @see org.openmrs.module.reporting.data.converter.DataConverter#convert(Object)
+	 * @should convert a Map into a String
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Object convert(Object original) {
+		if (original != null) {
+			StringBuilder ret = new StringBuilder();
+			for (Map.Entry<Object, Object> e : ((Map<Object, Object>) original).entrySet()) {
+				Object key = e.getKey();
+				if (keyConverter != null) {
+					key = keyConverter.convert(key);
+				}
+				Object val = e.getValue();
+				if (valueConverter != null) {
+					val = valueConverter.convert(val);
+				}
+				if (ret.length() > 0) {
+					ret.append(ObjectUtil.nvlStr(entrySeparator, ","));
+				}
+				ret.append(key).append(ObjectUtil.nvlStr(keyValueSeparator, ":")).append(val);
+			}
+			return ret.toString();
+		}
+		return null;
+	}
+
+	/**
+	 * @see org.openmrs.module.reporting.data.converter.DataConverter#getDataType()
+	 */
+	public Class<?> getDataType() {
+		return String.class;
+	}
+
+	/**
+	 * @see org.openmrs.module.reporting.data.converter.DataConverter#getInputDataType()
+	 */
+	public Class<?> getInputDataType() {
+		return Map.class;
+	}
+	
+	//***** PROPERTY ACCESS *****
+
+	public String getKeyValueSeparator() {
+		return keyValueSeparator;
+	}
+
+	public void setKeyValueSeparator(String keyValueSeparator) {
+		this.keyValueSeparator = keyValueSeparator;
+	}
+
+	public String getEntrySeparator() {
+		return entrySeparator;
+	}
+
+	public void setEntrySeparator(String entrySeparator) {
+		this.entrySeparator = entrySeparator;
+	}
+
+	public DataConverter getKeyConverter() {
+		return keyConverter;
+	}
+
+	public void setKeyConverter(DataConverter keyConverter) {
+		this.keyConverter = keyConverter;
+	}
+
+	public DataConverter getValueConverter() {
+		return valueConverter;
+	}
+
+	public void setValueConverter(DataConverter valueConverter) {
+		this.valueConverter = valueConverter;
+	}
+}
---
+++ api/src/test/java/org/openmrs/module/reporting/data/converter/MapConverterTest.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.reporting.data.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.module.reporting.common.DateUtil;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class MapConverterTest {
+
+	@Test
+	public void convert_shouldHandleKeyValueProperty() throws Exception {
+		MapConverter c = new MapConverter(" = ", null, null, null);
+		checkVal(c, "Key1 = Value1", "Key1", "Value1");
+	}
+
+	@Test
+	public void convert_shouldHandleEntryProperty() throws Exception {
+		MapConverter c = new MapConverter(": ", " | ", null, null);
+		checkVal(c, "Key1: Value1 | Key2: Value2", "Key1", "Value1", "Key2", "Value2");
+	}
+
+	@Test
+	public void convert_shouldHandleKeyConverter() throws Exception {
+		BooleanConverter bc = new BooleanConverter("oui", "non", "?");
+		MapConverter c = new MapConverter(" = ", " and ", bc, null);
+		checkVal(c, "oui = yes and non = no", Boolean.TRUE, "yes", Boolean.FALSE, "no");
+	}
+
+	@Test
+	public void convert_shouldHandleValueConverter() throws Exception {
+		BooleanConverter bc = new BooleanConverter("oui", "non", "?");
+		MapConverter c = new MapConverter(null, null, null, bc);
+		checkVal(c, "A1:oui,A2:non", "A1", Boolean.TRUE, "A2", Boolean.FALSE);
+	}
+
+	private void checkVal(MapConverter converter, String expected, Object...keyVals) {
+		Map<Object, Object> m = new LinkedHashMap<Object, Object>();
+		for (int i=0; i< keyVals.length; i+=2) {
+			m.put(keyVals[i], keyVals[i+1]);
+		}
+		Object val = converter.convert(m);
+		Assert.assertEquals(expected, val);
+	}
+}
