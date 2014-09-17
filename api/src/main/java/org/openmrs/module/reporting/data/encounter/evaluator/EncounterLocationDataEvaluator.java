5e307d297a4d489d3716c8f2de6e781fc1685cc6
mseaton <mseaton@pih.org>
2014-09-17 17:29:02 -0400

    Remove unused imports

--- api/src/main/java/org/openmrs/module/reporting/data/encounter/evaluator/EncounterLocationDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/reporting/data/encounter/evaluator/EncounterLocationDataEvaluator.java
@@ -1,19 +1,7 @@
 package org.openmrs.module.reporting.data.encounter.evaluator;
 
-import org.hibernate.Query;
-import org.hibernate.SessionFactory;
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.reporting.data.DataUtil;
-import org.openmrs.module.reporting.data.encounter.EncounterDataUtil;
-import org.openmrs.module.reporting.data.encounter.EvaluatedEncounterData;
-import org.openmrs.module.reporting.data.encounter.definition.EncounterDataDefinition;
 import org.openmrs.module.reporting.data.encounter.definition.EncounterLocationDataDefinition;
-import org.openmrs.module.reporting.evaluation.EvaluationContext;
-import org.openmrs.module.reporting.evaluation.EvaluationException;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.Set;
 
 /**
  * Evaluates a EncounterLocationDataDefinition to produce EncounterData
