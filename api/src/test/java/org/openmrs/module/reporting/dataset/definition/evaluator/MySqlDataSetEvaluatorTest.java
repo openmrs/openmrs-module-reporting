487e5c2ec8a6d4d09fd6c1b5c8d8130aa2123042
mseaton <mseaton@pih.org>
2014-09-16 22:45:20 -0400

    Missing @Ignore on inadvertently committed test

--- api/src/test/java/org/openmrs/module/reporting/dataset/definition/evaluator/MySqlDataSetEvaluatorTest.java
+++ api/src/test/java/org/openmrs/module/reporting/dataset/definition/evaluator/MySqlDataSetEvaluatorTest.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+@Ignore
 public class MySqlDataSetEvaluatorTest extends BaseModuleContextSensitiveTest {
 
 	@Autowired
