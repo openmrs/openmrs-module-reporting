d550ca2a761e1e5615d206b5633bcbdb3ad5ee56
mseaton <mseaton@pih.org>
2015-12-13 04:15:55 -0500

    REPORT-746:  Committing due to formatter changes to files.  No changes present.

---
+++ api/src/main/java/org/openmrs/module/reporting/cohort/definition/VisitCohortDefinition.java
@@ -0,0 +1,243 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.reporting.cohort.definition;
+
+import org.openmrs.Concept;
+import org.openmrs.Location;
+import org.openmrs.User;
+import org.openmrs.VisitType;
+import org.openmrs.module.reporting.common.Localized;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Eventually we will move this to a more broadly-shared module.
+ */
+@Localized("reporting.VisitCohortDefinition")
+public class VisitCohortDefinition extends BaseCohortDefinition {
+
+    @ConfigurationProperty(group = "which")
+    private List<VisitType> visitTypeList;
+
+    @ConfigurationProperty(group = "which")
+    private List<Location> locationList;
+
+    @ConfigurationProperty(group = "which")
+    private List<Concept> indicationList;
+
+    @ConfigurationProperty(group = "when")
+    private Date startedOnOrAfter;
+
+    @ConfigurationProperty(group = "when")
+    private Date startedOnOrBefore;
+
+    @ConfigurationProperty(group = "when")
+    private Date stoppedOnOrAfter;
+
+    @ConfigurationProperty(group = "when")
+    private Date stoppedOnOrBefore;
+
+    @ConfigurationProperty(group = "when")
+    private Date activeOnOrAfter;
+
+    @ConfigurationProperty(group = "when")
+    private Date activeOnOrBefore;
+
+    @ConfigurationProperty(group = "when")
+    private Boolean active;
+
+    @ConfigurationProperty(group = "other")
+    private Boolean returnInverse = Boolean.FALSE;
+
+    @ConfigurationProperty(group = "other")
+    private User createdBy;
+
+    @ConfigurationProperty(group = "other")
+    private Date createdOnOrBefore;
+
+    @ConfigurationProperty(group = "other")
+    private Date createdOnOrAfter;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Patients with visits");
+        if (startedOnOrAfter != null) {
+            sb.append(" started on or after " + startedOnOrAfter);
+        }
+        if (startedOnOrBefore != null) {
+            sb.append(" started on or before " + startedOnOrBefore);
+        }
+        if (stoppedOnOrAfter != null) {
+            sb.append(" stopped on or after " + stoppedOnOrAfter);
+        }
+        if (stoppedOnOrBefore != null) {
+            sb.append(" stopped on or before " + stoppedOnOrBefore);
+        }
+        if (activeOnOrAfter != null) {
+            sb.append(" active on or after " + activeOnOrAfter);
+        }
+        if (activeOnOrBefore != null) {
+            sb.append(" active on or before " + activeOnOrBefore);
+        }
+        if (active != null) {
+            if (active) {
+                sb.append(" and is active");
+            } else {
+                sb.append(" and is completed");
+            }
+        }
+        if (locationList != null) {
+            sb.append(" at " + locationList);
+        }
+        if (visitTypeList != null) {
+            sb.append(" of type " + visitTypeList);
+        }
+        if (indicationList != null) {
+            sb.append(" for reason " + indicationList);
+        }
+        if (returnInverse != null && returnInverse) {
+            sb.append(" AND INVERT THIS");
+        }
+        if (createdBy != null) {
+            sb.append(" created by " + createdBy);
+        }
+        if (createdOnOrAfter != null) {
+            sb.append(" created on or after " + createdOnOrAfter);
+        }
+        if (createdOnOrBefore != null) {
+            sb.append(" created on or before " + createdOnOrBefore);
+        }
+        return sb.toString();
+    }
+
+    public List<VisitType> getVisitTypeList() {
+        return visitTypeList;
+    }
+
+    public void setVisitTypeList(List<VisitType> visitTypeList) {
+        this.visitTypeList = visitTypeList;
+    }
+
+    public List<Location> getLocationList() {
+        return locationList;
+    }
+
+    public void setLocationList(List<Location> locationList) {
+        this.locationList = locationList;
+    }
+
+    public List<Concept> getIndicationList() {
+        return indicationList;
+    }
+
+    public void setIndicationList(List<Concept> indicationList) {
+        this.indicationList = indicationList;
+    }
+
+    public Date getStartedOnOrAfter() {
+        return startedOnOrAfter;
+    }
+
+    public void setStartedOnOrAfter(Date startedOnOrAfter) {
+        this.startedOnOrAfter = startedOnOrAfter;
+    }
+
+    public Date getStartedOnOrBefore() {
+        return startedOnOrBefore;
+    }
+
+    public void setStartedOnOrBefore(Date startedOnOrBefore) {
+        this.startedOnOrBefore = startedOnOrBefore;
+    }
+
+    public Date getStoppedOnOrAfter() {
+        return stoppedOnOrAfter;
+    }
+
+    public void setStoppedOnOrAfter(Date stoppedOnOrAfter) {
+        this.stoppedOnOrAfter = stoppedOnOrAfter;
+    }
+
+    public Date getStoppedOnOrBefore() {
+        return stoppedOnOrBefore;
+    }
+
+    public void setStoppedOnOrBefore(Date stoppedOnOrBefore) {
+        this.stoppedOnOrBefore = stoppedOnOrBefore;
+    }
+
+    public Date getActiveOnOrAfter() {
+        return activeOnOrAfter;
+    }
+
+    public void setActiveOnOrAfter(Date activeOnOrAfter) {
+        this.activeOnOrAfter = activeOnOrAfter;
+    }
+
+    public Date getActiveOnOrBefore() {
+        return activeOnOrBefore;
+    }
+
+    public void setActiveOnOrBefore(Date activeOnOrBefore) {
+        this.activeOnOrBefore = activeOnOrBefore;
+    }
+
+    public Boolean getReturnInverse() {
+        return returnInverse;
+    }
+
+    public void setReturnInverse(Boolean returnInverse) {
+        this.returnInverse = returnInverse;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedOnOrBefore() {
+        return createdOnOrBefore;
+    }
+
+    public void setCreatedOnOrBefore(Date createdOnOrBefore) {
+        this.createdOnOrBefore = createdOnOrBefore;
+    }
+
+    public Date getCreatedOnOrAfter() {
+        return createdOnOrAfter;
+    }
+
+    public void setCreatedOnOrAfter(Date createdOnOrAfter) {
+        this.createdOnOrAfter = createdOnOrAfter;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
---
+++ api/src/main/java/org/openmrs/module/reporting/cohort/definition/evaluator/VisitCohortDefinitionEvaluator.java
@@ -0,0 +1,78 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.reporting.cohort.definition.evaluator;
+
+import org.openmrs.Cohort;
+import org.openmrs.Visit;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.reporting.cohort.Cohorts;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.VisitCohortDefinition;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.HqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Handler(supports = VisitCohortDefinition.class)
+public class VisitCohortDefinitionEvaluator implements CohortDefinitionEvaluator {
+
+    @Autowired
+    EvaluationService evaluationService;
+
+    @Override
+    public EvaluatedCohort evaluate(CohortDefinition cohortDefinition, EvaluationContext context) throws EvaluationException {
+        VisitCohortDefinition cd = (VisitCohortDefinition) cohortDefinition;
+
+        HqlQueryBuilder q = new HqlQueryBuilder();
+        q.select("v.patient.id");
+        q.from(Visit.class, "v");
+        q.whereIn("v.visitType", cd.getVisitTypeList());
+        q.whereIn("v.location", cd.getLocationList());
+        q.whereIn("v.indication", cd.getIndicationList());
+        q.whereGreaterOrEqualTo("v.startDatetime", cd.getStartedOnOrAfter());
+        q.whereLessOrEqualTo("v.startDatetime", cd.getStartedOnOrBefore());
+        q.whereGreaterOrEqualTo("v.stopDatetime", cd.getStoppedOnOrAfter());
+        q.whereLessOrEqualTo("v.stopDatetime", cd.getStoppedOnOrBefore());
+        q.whereEqual("v.creator", cd.getCreatedBy());
+        q.whereGreaterOrEqualTo("v.dateCreated", cd.getCreatedOnOrAfter());
+        q.whereLessOrEqualTo("v.dateCreated", cd.getCreatedOnOrBefore());
+
+        if (cd.isActive() != null) {
+            q.whereNull("v.stopDatetime");
+        }
+        if (cd.getActiveOnOrAfter() != null) {
+            q.whereGreaterEqualOrNull("v.stopDatetime", cd.getActiveOnOrAfter());
+        }
+        if (cd.getActiveOnOrBefore() != null) {
+            q.whereLessOrEqualTo("v.startDatetime", cd.getActiveOnOrBefore());
+        }
+
+        q.wherePatientIn("v.patient.id", context);
+
+        // TODO need to exclude voided patients (just in case there are non-voided visits for voided patients)
+
+        Cohort c = new Cohort(evaluationService.evaluateToList(q, Integer.class, context));
+
+        if (cd.getReturnInverse()) {
+            Cohort baseCohort = Cohorts.allPatients(context);
+            c = Cohort.subtract(baseCohort, c);
+        }
+
+        return new EvaluatedCohort(c, cohortDefinition, context);
+    }
+
+}
