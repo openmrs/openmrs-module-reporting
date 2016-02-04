c729dd6ac015cea764d194165e382b4b128018b1
Daniel Kayiwa <kayiwadaniel@gmail.com>
2016-02-04 11:05:20 +0300

    Moving failing test to the api-tests sub module

--- api-tests/src/test/java/org/openmrs/module/reporting/cohort/definition/evaluator/VisitCohortDefinitionEvaluatorTest.java
+++ api-tests/src/test/java/org/openmrs/module/reporting/cohort/definition/evaluator/VisitCohortDefinitionEvaluatorTest.java
@@ -12,7 +12,7 @@
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 
-package org.openmrs.module.reporting.cohort.definition.evaluator;
+package src.test.java.org.openmrs.module.reporting.cohort.definition.evaluator;
 
 import org.junit.Before;
 import org.junit.Test;
