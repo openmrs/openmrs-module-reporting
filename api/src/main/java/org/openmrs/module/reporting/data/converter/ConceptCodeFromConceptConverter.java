9e2befd182b828090b4394330b47b87eedec4229
mogoodrich <mgoodrich@pih.org>
2016-05-23 16:21:53 -0400

    REPORT-766: Create ConceptCodeFromConcept converte

---
+++ api/src/main/java/org/openmrs/module/reporting/data/converter/ConceptCodeFromConceptConverter.java
@@ -0,0 +1,46 @@
+package org.openmrs.module.reporting.data.converter;
+
+import org.openmrs.Concept;
+import org.openmrs.ConceptMap;
+import org.openmrs.ConceptSource;
+
+public class ConceptCodeFromConceptConverter implements DataConverter {
+
+    private String conceptSourceName;
+
+    public ConceptCodeFromConceptConverter(String conceptSourceName) {
+        this.conceptSourceName = conceptSourceName;
+    }
+
+    public ConceptCodeFromConceptConverter(ConceptSource conceptSource) {
+        this.conceptSourceName = conceptSource.getName();
+    }
+
+    @Override
+    public Object convert(Object original) {
+
+        Concept concept = (Concept) original;
+
+        if (concept == null) {
+            return null;
+        }
+
+        for (ConceptMap map : concept.getConceptMappings()) {
+            // TODO right now assumes only one reference term per source, just returns first term found
+            if (map.getConceptReferenceTerm().getConceptSource().getName().equalsIgnoreCase(conceptSourceName)) {
+                return map.getConceptReferenceTerm().getCode();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?> getInputDataType() {
+        return Concept.class;
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return String.class;
+    }
+}
