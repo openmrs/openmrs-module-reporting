3c633bfaf565ac78bf1d70b9d91d9a48ac7fb65c
Michael Seaton <mseaton@pih.org>
2019-10-24 20:49:29 -0400

    REPORT-856 - Follow-up commit to resolve remaining 2.2 compatibility issues.

--- api-tests/females.groovy
+++
@@ -1,9 +0,0 @@
-import org.openmrs.module.reporting.cohort.definition.GenderCohortDefinition;
-
-class FemalesCohortDefinition extends GenderCohortDefinition {
-
-    public FemalesCohortDefinition() {
-        setFemaleIncluded(true);
-    }
-
-}
