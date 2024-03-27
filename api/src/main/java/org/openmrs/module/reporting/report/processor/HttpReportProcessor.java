b2ea989704bfc52676e8db825c4f74e1a5d56f59
Gift Mungule <GMungule@SmartCare.org.zm>
2024-03-27 22:18:16 +0200

    REPORT-862

--- api/src/main/java/org/openmrs/module/reporting/report/processor/HttpReportProcessor.java
+++ api/src/main/java/org/openmrs/module/reporting/report/processor/HttpReportProcessor.java
@@ -3,85 +3,78 @@ package org.openmrs.module.reporting.report.processor;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import org.json.simple.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.reporting.report.Report;
+import org.springframework.stereotype.Component;
 
+/**
+ * A ReportProcessor which sends the rendered report via HTTP POST
+ */
+@Component
 public class HttpReportProcessor implements ReportProcessor {
-    private String connectionUrl;
 
+    protected Log log = LogFactory.getLog(this.getClass());
 
-    public HttpReportProcessor(String connectionUrl) {
-        this.connectionUrl = connectionUrl;
-    }
-
-    @Override
+    /**
+     * @see ReportProcessor#getConfigurationPropertyNames()
+     */
     public List<String> getConfigurationPropertyNames() {
         List<String> ret = new ArrayList<>();
-        ret.add("connectionUrl");
-        ret.add("exportType");
-        ret.add("reportName");
-        ret.add("description");
+        ret.add("url");
+        ret.add("contentType");
         ret.add("dateFrom");
         ret.add("dateTo");
         return ret;
     }
 
-
-    @Override
+    /**
+     * Performs some action on the given report
+     * @param report the Report to process
+     */
     public void process(Report report, Properties configuration) {
-        this.connectionUrl = configuration.getProperty("connectionUrl");
 
         try {
-            // Establish connection to the URL
-            URL url = new URL(connectionUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-
-            // Write report data to the request body
-            String reportData = generateReportData(report, configuration);
-
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = reportData.getBytes(StandardCharsets.UTF_8);
-                os.write(input, 0, input.length);
+            String urlString = configuration.getProperty("url");
+            if (StringUtils.isBlank(urlString)) {
+                throw new IllegalArgumentException("URL cannot be blank");
             }
 
-            // Check connection response
-            int responseCode = connection.getResponseCode();
+            String contentType = configuration.getProperty("contentType", "text/plain");
+            String dateFrom = configuration.getProperty("dateFrom");
+            String dateTo = configuration.getProperty("dateTo");
+
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", contentType);
+
+            byte[] reportData = report.getRenderedOutput();
+
+            conn.setRequestProperty("Date-From", dateFrom);
+            conn.setRequestProperty("Date-To", dateTo);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                os.write(reportData);
+            }
+
+            int responseCode = conn.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
-
-                System.out.println("Report sent successfully via HTTP POST.");
+                log.info("Report sent successfully to " + urlString);
             } else {
-
-                System.out.println("Failed to send report. Response code: " + responseCode);
+                log.error("Failed to send report to " + urlString + ", HTTP error code: " + responseCode);
             }
-        } catch (Exception e) {
-            System.out.println("Error sending report via HTTP POST: " + e.getMessage());
+
+            conn.disconnect();
         }
-    }
-
-    private String generateReportData(Report report, Properties configuration) {
-
-        String exportType = configuration.getProperty("exportType");
-        String reportName = configuration.getProperty("reportName");
-        String description = configuration.getProperty("description");
-        String dateFrom = configuration.getProperty("dateFrom");
-        String dateTo = configuration.getProperty("dateTo");
-
-        // Construct JSON object with report properties
-        JSONObject data = new JSONObject();
-        data.put("exportType", exportType);
-        data.put("reportName", reportName);
-        data.put("description", description);
-        data.put("dateFrom", dateFrom);
-        data.put("dateTo", dateTo);
-
-        return data.toString();
+        catch (Exception e) {
+            throw new RuntimeException("Error occurred while sending report via HTTP", e);
+        }
     }
 }
