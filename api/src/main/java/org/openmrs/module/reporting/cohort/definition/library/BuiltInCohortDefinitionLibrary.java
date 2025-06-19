48d1946889e06c28a984db393a447b9e38163ab7
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-06-19 17:21:40 +0530

    REPORT-913: Migrate to OpenMRS Platform 2.4.0 (#263)
    * REPORT-913: Migrate to OpenMRS Platform 2.4.0 (#262)
    
    * Migrate to 2.4.0
    
    * Move api-1.9
    
    * Merge old api modules to the main
    
    * Set the scope of reportingcompatibility to provided
    
    (cherry picked from commit e4022f6a54ff5f4f2c0ddd0fdaf810a2f0b8d890)
    
    * Remove reporting compatability module
    
    * Remove old datasets

--- api/src/main/java/org/openmrs/module/reporting/cohort/definition/library/BuiltInCohortDefinitionLibrary.java
+++ api/src/main/java/org/openmrs/module/reporting/cohort/definition/library/BuiltInCohortDefinitionLibrary.java
@@ -19,6 +19,7 @@ import org.openmrs.ProgramWorkflowState;
 import org.openmrs.module.reporting.cohort.definition.AgeCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.BirthAndDeathCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.ConditionCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.EncounterCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.GenderCohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.InProgramCohortDefinition;
@@ -270,6 +271,21 @@ public class BuiltInCohortDefinitionLibrary extends BaseDefinitionLibrary<Cohort
         cd.addParameter(new Parameter("onOrAfter", "reporting.parameter.onOrAfter", Date.class));
         cd.addParameter(new Parameter("onOrBefore", "reporting.parameter.onOrBefore", Date.class));
         cd.addParameter(new Parameter("onDate", "reporting.parameter.date", Date.class));
+        return cd;
+    }
+
+    @DocumentedDefinition("conditonSearchAdvanced")
+    public CohortDefinition getConditonSearchAdvanced() {
+        ConditionCohortDefinition cd = new ConditionCohortDefinition();
+        cd.addParameter(new Parameter("conditionCoded", "reporting.parameter.conditionCoded", Concept.class));
+        cd.addParameter(new Parameter("conditionNonCoded", "reporting.parameter.conditionNonCoded", String.class));
+        cd.addParameter(new Parameter("onsetDateOnOrBefore", "reporting.parameter.onsetDateOnOrBefore", Date.class));
+        cd.addParameter(new Parameter("onsetDateOnOrAfter", "reporting.parameter.onsetDateOnOrAfter", Date.class));
+        cd.addParameter(new Parameter("endDateOnOrBefore", "reporting.parameter.endDateOnOrBefore", Date.class));
+        cd.addParameter(new Parameter("endDateOnOrAfter", "reporting.parameter.endDateOnOrAfter", Date.class));
+        cd.addParameter(new Parameter("createdOnOrBefore", "reporting.parameter.createdOnOrBefore", Date.class));
+        cd.addParameter(new Parameter("createdOnOrAfter", "reporting.parameter.createdOnOrAfter", Date.class));
+        cd.addParameter(new Parameter("activeOnDate", "reporting.parameter.activeOnDate", Date.class));
         return cd;
     }
 }
